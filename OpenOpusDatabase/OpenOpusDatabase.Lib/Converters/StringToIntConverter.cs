a980b083a71202d6c3621b358ee7a2667db5c7b5
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-07 11:35:09 +0100

    Create StringToIntConverter.cs

---
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/Converters/StringToIntConverter.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace OpenOpusDatabase.Lib.Converters
+{
+    internal class StringToIntConverter : JsonConverter<int>
+    {
+        public override int Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (typeToConvert == typeof(int))
+            {
+                return GetInt(reader);
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        private static int GetInt(Utf8JsonReader reader)
+        {
+            string? s = reader.GetString();
+            if (s != null)
+            {
+                return int.Parse(s);
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, int value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
