d8421a9d40ecb6e0819b8470372fe39236714fb0
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-07 11:35:01 +0100

    Create ComposerIdConverter.cs

---
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/Converters/ComposerIdConverter.cs
@@ -0,0 +1,48 @@
+﻿using OpenOpusDatabase.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace OpenOpusDatabase.Lib.Converters
+{
+    public class ComposerIdConverter : JsonConverter<int>
+    {
+        public override int Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (typeToConvert == typeof(int))
+            {
+                return GetId(reader, options);
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        private static int GetId(Utf8JsonReader reader, JsonSerializerOptions options)
+        {
+            Composer? composer;
+            using (JsonDocument jsonDocument = JsonDocument.ParseValue(ref reader))
+            {
+                composer = JsonSerializer.Deserialize<Composer>(jsonDocument, options);
+            }
+            if (composer != null)
+            {
+                return composer.Id;
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, int value, JsonSerializerOptions options)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
