d1ae54eb333bb420e83c052828dcbaeeeb7795e0
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-07 11:56:19 +0100

    Create WebReader.cs

---
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/APIFetching/WebReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenOpusDatabase.Lib.APIFetching
+{
+    internal static class WebReader
+    {
+        public static string DownloadText(string url)
+        {
+            string text;
+            using (HttpClient client = new())
+            {
+                HttpRequestMessage message = new(HttpMethod.Get, url);
+                HttpResponseMessage response = client.Send(message);
+                text = GetDataFromResponse(response);
+            }
+            return text;
+        }
+
+        private static string GetDataFromResponse(HttpResponseMessage response)
+        {
+            string text;
+            using (StreamReader reader = new(response.Content.ReadAsStream()))
+            {
+                text = reader.ReadToEnd();
+            }
+            return text;
+        }
+    }
+}
