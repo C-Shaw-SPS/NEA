3a034440f93670aa8b68004331dd7f10b126233a
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-07 11:35:22 +0100

    Create IResponse.cs

---
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/Responses/IResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenOpusDatabase.Lib.Responses
+{
+    public interface IResponse<T>
+    {
+        public List<T> Values { get; set; }
+    }
+}
