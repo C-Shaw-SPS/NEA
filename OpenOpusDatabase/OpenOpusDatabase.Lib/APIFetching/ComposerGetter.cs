0d6b59bcc163aeebfed12c8d35299d46f68a5e07
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-07 11:56:14 +0100

    Create ComposerGetter.cs

---
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/APIFetching/ComposerGetter.cs
@@ -0,0 +1,24 @@
+﻿using OpenOpusDatabase.Lib.Models;
+using OpenOpusDatabase.Lib.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenOpusDatabase.Lib.APIFetching
+{
+    public static class ComposerGetter
+    {
+        private const string _URL = "https://api.openopus.org/composer/list/search/.json";
+        public static List<Composer> GetFromOpenOpus()
+        {
+            return JsonGetter.GetFromUrl<Composer, ComposerResponse>(_URL);
+        }
+
+        public static List<Composer> GetFromFile(string filePath)
+        {
+            return JsonGetter.GetFromFile<Composer, ComposerResponse>(filePath);
+        }
+    }
+}
