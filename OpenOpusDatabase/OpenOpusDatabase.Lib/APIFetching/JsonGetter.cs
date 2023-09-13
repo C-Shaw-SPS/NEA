d13bff19f22c778f239a453eac0d39f0769fd38e
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-13 12:09:53 +0100

    Update JsonGetter.cs

--- OpenOpusDatabase/OpenOpusDatabase.Lib/APIFetching/JsonGetter.cs
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/APIFetching/JsonGetter.cs
@@ -10,29 +10,29 @@ namespace OpenOpusDatabase.Lib.APIFetching
 {
     internal static class JsonGetter
     {
-        public static List<T> GetValuesFromResponse<T, TResponse>(string json, JsonSerializerOptions options = null) where TResponse : IResponse<T>
+        public static List<T> GetValuesFromResponse<T, TResponse>(string json) where TResponse : IResponse<T>
         {
-            TResponse? response = JsonSerializer.Deserialize<TResponse>(json, options);
+            TResponse? response = JsonSerializer.Deserialize<TResponse>(json);
             if (response != null)
             {
                 return response.Values;
             }
             else
             {
-                throw new ArgumentNullException();
+                throw new Exception("null response");
             }
         }
 
-        public static List<T> GetFromFile<T, TResponse>(string filePath, JsonSerializerOptions options = null) where TResponse : IResponse<T>
+        public static List<T> GetFromFile<T, TResponse>(string filePath) where TResponse : IResponse<T>
         {
             string json = File.ReadAllText(filePath);
-            return GetValuesFromResponse<T, TResponse>(json, options);
+            return GetValuesFromResponse<T, TResponse>(json);
         }
 
-        public static List<T> GetFromUrl<T, TResponse>(string url, JsonSerializerOptions options = null) where TResponse : IResponse<T>
+        public static List<T> GetFromUrl<T, TResponse>(string url) where TResponse : IResponse<T>
         {
             string json = WebReader.DownloadText(url);
-            return GetValuesFromResponse<T, TResponse>(json, options);
+            return GetValuesFromResponse<T, TResponse>(json);
         }
     }
 }
