27bb0d0d0931be463c4d7199ec92a55a71aba67d
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-14 10:59:28 +0100

    Create WorkTests.cs

---
+++ OpenOpusDatabase/OpenOpusDatabase.Tests/WorkTests.cs
@@ -0,0 +1,27 @@
+﻿using OpenOpusDatabase.Lib.Models;
+using OpenOpusDatabase.Lib.APIFetching;
+
+namespace OpenOpusDatabase.Tests
+{
+    public class WorkTests
+    {
+        const string FILE_PATH = "work.json";
+
+        readonly Work expectedWork = new()
+        {
+            Id = 20086,
+            ComposerId = 176,
+            Title = "3 Movements",
+            Subtitle = "",
+            Genre = "Orchestral",
+        };
+
+        [Fact]
+        public void TestJsonDeserialiseWork()
+        {
+            Work? actualWork = JsonGetter.GetFromFile<Work>(FILE_PATH); 
+            Assert.NotNull(actualWork);
+            Assert.Equal(expectedWork, actualWork);
+        }
+    }
+}
