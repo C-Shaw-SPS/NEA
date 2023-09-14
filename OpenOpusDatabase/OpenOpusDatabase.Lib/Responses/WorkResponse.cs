1ab729e766194665918d9b069cf4fad42fd92e5a
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-14 11:33:35 +0100

    Create WorkResponse.cs

---
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/Responses/WorkResponse.cs
@@ -0,0 +1,29 @@
+﻿using OpenOpusDatabase.Lib.Converters;
+using OpenOpusDatabase.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace OpenOpusDatabase.Lib.Responses
+{
+    public class WorkResponse : IResponse<Work>
+    {
+        private List<Work> _data;
+
+        [JsonPropertyName("works"), JsonConverter(typeof(DictionaryToListConverter<string, Work>))]
+        public List<Work> Values
+        {
+            get
+            {
+                return _data;
+            }
+            set
+            {
+                _data = value;
+            }
+        }
+    }
+}
