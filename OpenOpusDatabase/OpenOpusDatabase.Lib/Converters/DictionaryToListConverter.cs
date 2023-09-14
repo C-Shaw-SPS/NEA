b889b1569f78673760c8616023a8ab32b711edc5
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-14 11:17:43 +0100

    Create DictionaryToListConverter.cs

---
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/Converters/DictionaryToListConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace OpenOpusDatabase.Lib.Converters
+{
+    public class DictionaryToListConverter<TKey, TValue> : JsonConverter<List<TValue>>
+    {
+        public override List<TValue>? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, List<TValue> value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
