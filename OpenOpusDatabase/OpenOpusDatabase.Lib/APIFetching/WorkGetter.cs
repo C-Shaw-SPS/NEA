1ecab73ad4732da87819ba380afc59bea8bdfcac
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-14 11:33:32 +0100

    Create WorkGetter.cs

---
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/APIFetching/WorkGetter.cs
@@ -0,0 +1,25 @@
+﻿using OpenOpusDatabase.Lib.Models;
+using OpenOpusDatabase.Lib.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenOpusDatabase.Lib.APIFetching
+{
+    public class WorkGetter
+    {
+        private const string _URL = "https://api.openopus.org/work/list/ids/.json";
+
+        public static List<Work> GetFromOpenOpus()
+        {
+            return JsonGetter.GetFromUrl<Work, WorkResponse>(_URL);
+        }
+
+        public static List<Work> GetFromFile(string filePath)
+        {
+            return JsonGetter.GetFromFile<Work, WorkResponse>(filePath);
+        }
+    }
+}
