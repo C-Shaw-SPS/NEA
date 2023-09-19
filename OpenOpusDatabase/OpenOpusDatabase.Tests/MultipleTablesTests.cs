29f074c6ffa0db12748a22caf7b3642811a02222
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-19 15:32:11 +0100

    Create MultipleTablesTests.cs

---
+++ OpenOpusDatabase/OpenOpusDatabase.Tests/MultipleTablesTests.cs
@@ -0,0 +1,65 @@
+﻿using OpenOpusDatabase.Lib.Databases;
+using OpenOpusDatabase.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenOpusDatabase.Tests
+{
+    public class MultipleTablesTests
+    {
+        [Fact]
+        public async void TestAddComposersAndWorks()
+        {
+            Database<Composer> composerDatabase = new(nameof(TestAddComposersAndWorks));
+            Database<Work> workDatabase = new(nameof(TestAddComposersAndWorks));
+
+            await composerDatabase.ClearAsync();
+            await workDatabase.ClearAsync();
+
+            await composerDatabase.InsertAllAsync(Expected.Composers);
+            await workDatabase.InsertAllAsync(Expected.Works);
+
+            List<Composer> actualComposers = await composerDatabase.GetAllAsync();
+            List<Work> actualWorks = await workDatabase.GetAllAsync();
+
+            foreach (Composer expectedComposer in Expected.Composers)
+            {
+                Assert.Contains(expectedComposer, actualComposers);
+            }
+
+            foreach (Work expectedWork in Expected.Works)
+            {
+                Assert.Contains(expectedWork, actualWorks);
+            }
+        }
+
+        [Fact]
+        public async void TestClearOneTable()
+        {
+            Database<Composer> composerDatabase = new(nameof(TestClearOneTable));
+            Database<Work> workDatabase = new(nameof(TestClearOneTable));
+
+            await composerDatabase.ClearAsync();
+            await workDatabase.ClearAsync();
+
+            await composerDatabase.InsertAllAsync(Expected.Composers);
+            await workDatabase.InsertAllAsync(Expected.Works);
+
+            await composerDatabase.ClearAsync();
+
+            List<Composer> actualComposers = await composerDatabase.GetAllAsync();
+            List<Work> actualWorks = await workDatabase.GetAllAsync();
+
+            Assert.Empty(actualComposers);
+
+            foreach (Work expectedWork in Expected.Works)
+            {
+                Assert.Contains(expectedWork, actualWorks);
+            }
+        }
+    }
+}
