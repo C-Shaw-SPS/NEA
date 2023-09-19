60ae3ead25e146873902c462a6180ecb89b2ea93
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-19 15:31:00 +0100

    Update Program.cs

--- OpenOpusDatabase/OpenOpusDatabase.App/Program.cs
+++ OpenOpusDatabase/OpenOpusDatabase.App/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace OpenOpusDatabase.App
+﻿using OpenOpusDatabase.Lib.APIFetching;
+using OpenOpusDatabase.Lib.Databases;
+using OpenOpusDatabase.Lib.Models;
+
+namespace OpenOpusDatabase.App
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            await CreateComposerDatabase();
+            Console.WriteLine("Composer database created");
+            await CreateWorkDatabase();
+            Console.WriteLine("Work database created");
+        }
+
+        static async Task CreateComposerDatabase()
+        {
+            List<Composer> composers = ComposerGetter.GetFromOpenOpus();
+            Database<Composer> composerDatabase = new(DatabaseProperties.NAME);
+            await composerDatabase.ClearAsync();
+            await composerDatabase.InsertAllAsync(composers);
+        }
+
+        static async Task CreateWorkDatabase()
+        {
+            List<Work> works = WorkGetter.GetFromOpenOpus();
+            Database<Work> workDatabase = new(DatabaseProperties.NAME);
+            await workDatabase.ClearAsync();
+            await workDatabase.InsertAllAsync(works);
         }
     }
 }
