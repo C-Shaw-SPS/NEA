c2af1be70de1b5cb9fdc69849f4eba317c1e0c72
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-19 08:30:59 +0100

    Create DatabaseProperties.cs

---
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/Databases/DatabaseProperties.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenOpusDatabase.Lib.Databases
+{
+    public static class DatabaseProperties
+    {
+        public const string NAME = "CluedoDatabase.db";
+
+        public const SQLiteOpenFlags FLAGS =
+            SQLiteOpenFlags.ReadWrite |
+            SQLiteOpenFlags.Create |
+            SQLiteOpenFlags.SharedCache;
+    }
+}
