50cdf1ce81897767f4484a173b722a3483815609
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-19 08:31:01 +0100

    Create Work.cs

---
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/Models/Work.cs
@@ -0,0 +1,90 @@
+﻿using OpenOpusDatabase.Lib.Converters;
+using SQLite;
+using System.Text.Json.Serialization;
+
+namespace OpenOpusDatabase.Lib.Models
+{
+    [Table("Works")]
+    public class Work : IIdentifiable, IEquatable<Work>
+    {
+        private int _id;
+        private int _composerId;
+        private string _title = string.Empty;
+        private string _subtitle = string.Empty;
+        private string _genre = string.Empty;
+
+        [PrimaryKey, AutoIncrement, JsonPropertyName("id"), JsonConverter(typeof(StringToIntConverter))]
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        [JsonPropertyName("composer"), JsonConverter(typeof(ComposerIdConverter))]
+        public int ComposerId
+        {
+            get
+            {
+                return _composerId;
+            }
+            set
+            {
+                _composerId = value;
+            }
+        }
+
+        [JsonPropertyName("title")]
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+            }
+        }
+
+        [JsonPropertyName("subtitle")]
+        public string Subtitle
+        {
+            get
+            {
+                return _subtitle;
+            }
+            set
+            {
+                _subtitle = value;
+            }
+        }
+
+        [JsonPropertyName("genre")]
+        public string Genre
+        {
+            get
+            {
+                return _genre;
+            }
+            set
+            {
+                _genre = value;
+            }
+        }
+
+        public bool Equals(Work? other)
+        {
+            return other != null
+                && _composerId == other._composerId
+                && _title == other._title
+                && _subtitle == other._subtitle
+                && _genre == other._genre;
+        }
+    }
+}
