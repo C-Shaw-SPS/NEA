3064fc97d18572e33197a97eb3e793418be210b0
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-20 13:30:59 +0100

    Create TableName.cs
    Gets the table name of a type

---
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/Databases/TableName.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenOpusDatabase.Lib.Databases
+{
+    internal static class TableName
+    {
+        public static string GetTableName<T>()
+        {
+            string tableName = typeof(T).Name;
+            object[] customAttributes = typeof(T).GetCustomAttributes(typeof(TableAttribute), false);
+            if (customAttributes.Length > 0)
+            {
+                TableAttribute? tableAttribute = customAttributes[0] as TableAttribute;
+                if (tableAttribute != null)
+                {
+                    tableName = tableAttribute.Name;
+                }
+            }
+            return tableName;
+        }
+    }
+}
