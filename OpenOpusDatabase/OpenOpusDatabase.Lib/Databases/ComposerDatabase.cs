a35eb67db61ac972ee36bf699efd11350c808d66
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-21 11:39:59 +0100

    InsertAsync SQL

--- OpenOpusDatabase/OpenOpusDatabase.Lib/Databases/ComposerDatabase.cs
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/Databases/ComposerDatabase.cs
@@ -1,4 +1,5 @@
 ﻿using OpenOpusDatabase.Lib.Models;
+using System.Data.Common;
 
 namespace OpenOpusDatabase.Lib.Databases
 {
@@ -7,6 +8,16 @@ namespace OpenOpusDatabase.Lib.Databases
         public ComposerDatabase(string path) : base(path)
         {
 
+        }
+
+        public override async Task InsertAsync(Composer value)
+        {
+            await InitAsync();
+            string command = $"""
+                INSERT INTO {TableName}
+                VALUES ({value.Id}, "{value.Name}", "{value.CompleteName}", {value.BirthDate.Ticks}, {value.DeathDate?.Ticks}, "{value.Era}", "{value.PortraitLink}")
+                """;
+            await Connection.ExecuteAsync(command);
         }
     }
 }
