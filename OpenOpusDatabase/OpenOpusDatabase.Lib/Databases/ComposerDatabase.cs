9afd8eb8565beaa090574ef3f91001eecfaeb645
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-21 11:17:02 +0100

    Create ComposerDatabase.cs

---
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/Databases/ComposerDatabase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenOpusDatabase.Lib.Databases
+{
+    internal class ComposerDatabase
+    {
+    }
+}
