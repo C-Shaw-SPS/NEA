fce4e5b9eb6b13842b338c3bd4a2e7214752ad67
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-21 11:17:26 +0100

    Update TableName.cs

--- OpenOpusDatabase/OpenOpusDatabase.Lib/Databases/TableName.cs
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/Databases/TableName.cs
@@ -4,14 +4,16 @@ namespace OpenOpusDatabase.Lib.Databases
 {
     internal static class TableName
     {
-        public static string GetTableName<T>()
+        /// <summary>
+        /// Returns the table name of the specified type, returning the name of the type if the type does not have the TableAttribute attribute
+        /// </summary>
+        public static string Get<T>()
         {
             string tableName = typeof(T).Name;
             object[] customAttributes = typeof(T).GetCustomAttributes(typeof(TableAttribute), false);
             if (customAttributes.Length > 0)
             {
-                TableAttribute? tableAttribute = customAttributes[0] as TableAttribute;
-                if (tableAttribute != null)
+                if (customAttributes[0] is TableAttribute tableAttribute)
                 {
                     tableName = tableAttribute.Name;
                 }
