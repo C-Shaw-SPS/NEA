273f53ddca8b035a372993f842d79eb86bfb44b0
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-21 11:23:17 +0100

    Update ComposerDatabase.cs

--- OpenOpusDatabase/OpenOpusDatabase.Lib/Databases/ComposerDatabase.cs
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/Databases/ComposerDatabase.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenOpusDatabase.Lib.Models;
 
 namespace OpenOpusDatabase.Lib.Databases
 {
-    internal class ComposerDatabase
+    public class ComposerDatabase : Database<Composer>
     {
+        public ComposerDatabase(string path) : base(path)
+        {
+
+        }
     }
 }
