41039a7fb149cb504b9643657e28419624492c3b
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-09-25 13:21:50 +0100

    Create SqlFormatting.cs

---
+++ OpenOpusDatabase/OpenOpusDatabase.Lib/Databases/SqlFormatting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenOpusDatabase.Lib.Databases
+{
+    internal static class SqlFormatting
+    {
+        public static string CommaJoin(this List<string> list)
+        {
+            return "(" + String.Join(", ", list) + ")";
+        }
+    }
+}
