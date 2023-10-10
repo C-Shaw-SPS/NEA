31e17e758d56281dc573e96da389261de9af8b14
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-10-10 12:40:38 +0100

    Create Pupil.cs

---
+++ MusicOrganisationTests/MusicOrganisationTests.Lib/Models/Pupil.cs
@@ -0,0 +1,89 @@
+﻿using MusicOrganisationTests.Lib.Databases;
+using SQLite;
+
+namespace MusicOrganisationTests.Lib.Models
+{
+    [Table(_TABLE_NAME)]
+    public class Pupil : ISqlStorable
+    {
+        private const string _TABLE_NAME = "Pupils";
+
+        private int _id;
+        private string _name = string.Empty;
+        private string _level = string.Empty;
+        private TimeSpan _lessonDuration;
+
+        public static string TableName => _TABLE_NAME;
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string Level
+        {
+            get
+            {
+                return _level;
+            }
+            set
+            {
+                _level = value;
+            }
+        }
+
+        public TimeSpan LessonDuration
+        {
+            get
+            {
+                return _lessonDuration;
+            }
+            set
+            {
+                _lessonDuration = value;
+            }
+        }
+
+        public static IEnumerable<string> GetColumnNames()
+        {
+            return new List<string>
+            {
+                nameof(Id),
+                nameof(Name),
+                nameof(Level),
+                nameof(LessonDuration)
+            };
+        }
+
+        public IEnumerable<string> GetSqlValues()
+        {
+            List<object?> values = new()
+            {
+                _id,
+                _name,
+                _level,
+                _lessonDuration
+            };
+            return values.FormatAsSqlValues();
+        }
+    }
+}
