4facf9099a22932c032fda3e22cb5419ca1a6ffa
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-10-10 12:40:47 +0100

    Create Timetable.cs

---
+++ MusicOrganisationTests/MusicOrganisationTests.Lib/Timetabling/Timetable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisationTests.Lib.Timetabling
+{
+    public class Timetable
+    {
+
+    }
+}
