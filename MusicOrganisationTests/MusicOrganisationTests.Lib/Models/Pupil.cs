bc5e26b6ccb652683f8bcd06d123169739cbba39
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-10-12 11:41:53 +0100

    add data
    lesson day, different times, email, phone number

--- MusicOrganisationTests/MusicOrganisationTests.Lib/Models/Pupil.cs
+++ MusicOrganisationTests/MusicOrganisationTests.Lib/Models/Pupil.cs
@@ -1,10 +1,11 @@
 ﻿using MusicOrganisationTests.Lib.Databases;
+using MusicOrganisationTests.Lib.Timetabling;
 using SQLite;
 
 namespace MusicOrganisationTests.Lib.Models
 {
     [Table(_TABLE_NAME)]
-    public class Pupil : ISqlStorable
+    public class Pupil : IEquatable<Pupil>, ISqlStorable
     {
         private const string _TABLE_NAME = "Pupils";
 
@@ -12,6 +13,10 @@ namespace MusicOrganisationTests.Lib.Models
         private string _name = string.Empty;
         private string _level = string.Empty;
         private TimeSpan _lessonDuration;
+        private Day _lessonDay;
+        private bool _differentTimes;
+        private string? _email;
+        private string? _phoneNumber;
 
         public static string TableName => _TABLE_NAME;
 
@@ -63,6 +68,66 @@ namespace MusicOrganisationTests.Lib.Models
             }
         }
 
+        public Day LessonDay
+        {
+            get
+            {
+                return _lessonDay;
+            }
+            set
+            {
+                _lessonDay = value;
+            }
+        }
+
+        public bool DifferentTimes
+        {
+            get
+            {
+                return _differentTimes;
+            }
+            set
+            {
+                _differentTimes = value;
+            }
+        }
+
+        public string? Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+
+        public string? PhoneNumber
+        {
+            get
+            {
+                return _phoneNumber;
+            }
+            set
+            {
+                _phoneNumber = value;
+            }
+        }
+
+        public bool Equals(Pupil? other)
+        {
+            return other != null
+                && _id == other._id
+                && _name == other._name
+                && _level == other._level
+                && _lessonDay == other._lessonDay
+                && _differentTimes == other._differentTimes
+                && _email == other._email
+                && _phoneNumber == other._phoneNumber;
+        }
+
         public static IEnumerable<string> GetColumnNames()
         {
             return new List<string>
@@ -70,20 +135,25 @@ namespace MusicOrganisationTests.Lib.Models
                 nameof(Id),
                 nameof(Name),
                 nameof(Level),
-                nameof(LessonDuration)
+                nameof(LessonDuration),
+                nameof(LessonDay),
+                nameof(DifferentTimes),
+                nameof(Email),
+                nameof(PhoneNumber)
             };
         }
 
         public IEnumerable<string> GetSqlValues()
         {
-            List<object?> values = new()
-            {
+            return SqlFormatting.FormatAsSqlValues(
                 _id,
                 _name,
                 _level,
-                _lessonDuration
-            };
-            return values.FormatAsSqlValues();
+                _lessonDuration,
+                _lessonDay,
+                _differentTimes,
+                _email,
+                _phoneNumber);
         }
     }
 }
