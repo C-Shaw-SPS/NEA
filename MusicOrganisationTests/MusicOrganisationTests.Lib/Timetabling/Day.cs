8404519664f578458976385f150287a020795a00
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-10-12 11:40:48 +0100

    Create Day.cs

---
+++ MusicOrganisationTests/MusicOrganisationTests.Lib/Timetabling/Day.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisationTests.Lib.Timetabling
+{
+    public enum Day
+    {
+        Monday = 0b0000001,
+        Tuesday = 0b0000010,
+        Wednesday = 0b0000100,
+        Thursday = 0b0001000,
+        Friday = 0b0010000,
+        Saturday = 0b0100000,
+        Sunday = 0b1000000
+    }
+}
