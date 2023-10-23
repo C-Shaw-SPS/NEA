7fef00581c5007a1695f3e05247c5ab58861d285
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-10-23 10:46:26 +0100

    Create RepertoireTests.cs

---
+++ MusicOrganisationTests/MusicOrganisationTests.Tests/Models/RepertoireTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisationTests.Tests.Models
+{
+    public class RepertoireTests
+    {
+        [Fact]
+        public async Task TestRepertoireSql()
+        {
+
+        }
+    }
+}
