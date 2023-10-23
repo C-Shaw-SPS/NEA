ae86aa170a74597cbb98039ab56c831b78b7fc43
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-10-23 12:21:58 +0100

    Update Repertoire.cs

--- MusicOrganisationTests/MusicOrganisationTests.Lib/Models/Repertoire.cs
+++ MusicOrganisationTests/MusicOrganisationTests.Lib/Models/Repertoire.cs
@@ -1,11 +1,6 @@
 ﻿using MusicOrganisationTests.Lib.Databases;
 using MusicOrganisationTests.Lib.Enums;
 using SQLite;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MusicOrganisationTests.Lib.Models
 {
