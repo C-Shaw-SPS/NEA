b3da4d0211c219db4be3ec1966bbda92b6605e78
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-10-23 10:42:50 +0100

    move to enums

--- MusicOrganisationTests/MusicOrganisationTests.Lib/Enums/Day.cs
+++ MusicOrganisationTests/MusicOrganisationTests.Lib/Enums/Day.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MusicOrganisationTests.Lib.Timetabling
+namespace MusicOrganisationTests.Lib.Enums
 {
     public enum Day
     {
