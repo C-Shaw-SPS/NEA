a16a103b3403bba812ddb123df08571736d0c999
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-10-30 13:41:31 +0000

    add tests for caregivers and lessons

--- MusicOrganisationTests/MusicOrganisationTests.Tests/Models/SqlTests.cs
+++ MusicOrganisationTests/MusicOrganisationTests.Tests/Models/SqlTests.cs
@@ -11,38 +11,56 @@ namespace MusicOrganisationTests.Tests.Models
     public class SqlTests
     {
         [Fact]
+        public async Task TestCaregiverSql()
+        {
+            await TestType(Expected.Caregivers);
+        }
+
+        [Fact]
+        public async Task TestCaregiverMapSql()
+        {
+            await TestType(Expected.CaregiverMaps);
+        }
+
+        [Fact]
         public async Task TestComposerSql()
         {
-            await TestType(Expected.Composers, nameof(TestComposerSql));
+            await TestType(Expected.Composers);
+        }
+
+        [Fact]
+        public async Task TestLessonSql()
+        {
+            await TestType(Expected.Lessons);
+        }
+
+        [Fact]
+        public async Task TestLessonTimeSql()
+        {
+            await TestType(Expected.LessonTimes);
         }
 
         [Fact]
         public async Task TestWorkSql()
         {
-            await TestType(Expected.Works, nameof(TestWorkSql));
+            await TestType(Expected.Works);
         }
 
         [Fact]
         public async Task TestRepertoireSql()
         {
-            await TestType(Expected.Repertoires, nameof(TestRepertoireSql));
-        }
-
-        [Fact]
-        public async Task TestCaregiverSql()
-        {
-            await TestType(Expected.Caregivers, nameof(TestCaregiverSql));
+            await TestType(Expected.Repertoires);
         }
 
         [Fact]
         public async Task TestPupilSql()
         {
-            await TestType(Expected.Pupils, nameof(TestPupilSql));
+            await TestType(Expected.Pupils);
         }
 
-        private async static Task TestType<T>(IEnumerable<T> expectedItems, string databaseName) where T : class, ISqlStorable, new()
+        private async static Task TestType<T>(IEnumerable<T> expectedItems) where T : class, ISqlStorable, new()
         {
-            TableConnection<T> table = new(databaseName);
+            TableConnection<T> table = new(nameof(SqlTests));
             await table.ClearAsync();
             await table.InsertAllAsync(expectedItems);
             IEnumerable<T> actual = await table.GetAllAsync();
