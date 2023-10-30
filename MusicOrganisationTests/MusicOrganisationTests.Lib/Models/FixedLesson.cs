dbf31e792f770a19104ebe1552c2964b44f3583a
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-10-30 13:47:19 +0000

    Create FixedLesson.cs

---
+++ MusicOrganisationTests/MusicOrganisationTests.Lib/Models/FixedLesson.cs
@@ -0,0 +1,32 @@
+﻿using MusicOrganisationTests.Lib.Databases;
+using SQLite;
+
+namespace MusicOrganisationTests.Lib.Models
+{
+    [Table(_TABLE_NAME)]
+    public class FixedLesson : ISqlStorable, IEquatable<FixedLesson>
+    {
+        private const string _TABLE_NAME = "FixedLessons";
+
+
+
+        public static string TableName => _TABLE_NAME;
+
+        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public static IEnumerable<string> GetColumnNames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetSqlValues()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(FixedLesson? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
