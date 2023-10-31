c0270b6d5b6b11c268418dadbc4ec254015bde48
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-10-31 15:26:06 +0000

    Create ComposerService.cs

---
+++ MusicOrganisationTests/MusicOrganisationTests.Lib/Services/ComposerService.cs
@@ -0,0 +1,17 @@
+﻿using MusicOrganisationTests.Lib.Databases;
+using MusicOrganisationTests.Lib.Models;
+
+namespace MusicOrganisationTests.Lib.Services
+{
+    public class ComposerService
+    {
+        private TableConnection<Composer> _composerTable;
+
+        public ComposerService(string path)
+        {
+            _composerTable = new(path);
+        }
+
+
+    }
+}
