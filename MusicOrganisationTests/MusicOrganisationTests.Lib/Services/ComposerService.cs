677cd1da84fcdbd0fc3ceeac8e8681a24a11dbbe
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-10-31 15:59:45 +0000

    Update ComposerService.cs

--- MusicOrganisationTests/MusicOrganisationTests.Lib/Services/ComposerService.cs
+++ MusicOrganisationTests/MusicOrganisationTests.Lib/Services/ComposerService.cs
@@ -1,4 +1,5 @@
-﻿using MusicOrganisationTests.Lib.Databases;
+﻿using MusicOrganisationTests.Lib.APIFetching;
+using MusicOrganisationTests.Lib.Databases;
 using MusicOrganisationTests.Lib.Models;
 
 namespace MusicOrganisationTests.Lib.Services
@@ -10,6 +11,27 @@ namespace MusicOrganisationTests.Lib.Services
         public ComposerService(string path)
         {
             _composerTable = new(path);
+        }
+
+        public async Task InitialiseData()
+        {
+            IEnumerable<Composer> composers = ComposerGetter.GetFromOpenOpus();
+            await _composerTable.InsertAllAsync(composers);
+        }
+
+        public async Task AddComposer(string name, string completeName, DateTime birthDate, DateTime deathDate, string era, string? portraitLink = null)
+        {
+            int id = await _composerTable.GetNextIdAsync();
+            Composer composer = new()
+            {
+                Id = id,
+                Name = name,
+                CompleteName = completeName,
+                BirthDate = birthDate,
+                DeathDate = deathDate,
+                PortraitLink = portraitLink
+            };
+            await _composerTable.InsertAsync(composer);
         }
     }
 }
