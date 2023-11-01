653c78ea7b6c91f6ac8e8e7b3e10271200bd7471
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-11-01 13:31:40 +0000

    Create ComposerServiceTests.cs

---
+++ MusicOrganisationTests/MusicOrganisationTests.Tests/Services/ComposerServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using MusicOrganisationTests.Lib.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisationTests.Tests.Services
+{
+    public class ComposerServiceTests
+    {
+        [Fact]
+        public async Task TestAddComposer()
+        {
+            ComposerService service = new(nameof(TestAddComposer));
+            await service.Add(
+                Expected.Composers[0].Name,
+                Expected.Composers[0].CompleteName,
+                Expected.Composers[0].BirthDate,
+                Expected.Composers[0].DeathDate,
+                Expected.Composers[0].Era,
+                Expected.Composers[0].PortraitLink
+                );
+
+        }
+    }
+}
