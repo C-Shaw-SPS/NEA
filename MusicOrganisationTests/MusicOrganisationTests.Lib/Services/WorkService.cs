e88287b83122ae7c140d3b04b3a17d9e25e28567
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-11-01 13:31:29 +0000

    Create WorkService.cs

---
+++ MusicOrganisationTests/MusicOrganisationTests.Lib/Services/WorkService.cs
@@ -0,0 +1,35 @@
+﻿using MusicOrganisationTests.Lib.APIFetching;
+using MusicOrganisationTests.Lib.Models;
+
+namespace MusicOrganisationTests.Lib.Services
+{
+    public class WorkService : Service<Work>
+    {
+        public WorkService(string path) : base(path) { }
+
+        public async Task InitialiseData()
+        {
+            IEnumerable<Work> works = WorkGetter.GetFromOpenOpus();
+            await _table.InsertAllAsync(works);
+        }
+
+        public async Task Add(int composerId, string title, string subtitle, string genre)
+        {
+            int id = await _table.GetNextIdAsync();
+            Work work = new()
+            {
+                Id = id,
+                ComposerId = composerId,
+                Title = title,
+                Subtitle = subtitle,
+                Genre = genre
+            };
+            await _table.InsertAsync(work);
+        }
+
+        public async Task<IEnumerable<Work>> GetFromComposer(Composer composer)
+        {
+            return await _table.GetWhereEqualAsync(nameof(Work.ComposerId), composer.Id);
+        }
+    }
+}
