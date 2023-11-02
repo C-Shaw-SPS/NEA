8acaeb814fc4fdce0874037661c087fe27b921aa
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-11-02 13:44:14 +0000

    Update Program.cs

--- MusicOrganisationTests/MusicOrganisationTests.App/Program.cs
+++ MusicOrganisationTests/MusicOrganisationTests.App/Program.cs
@@ -11,8 +11,31 @@ namespace MusicOrganisationTests.App
         static async Task Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            await CreateEmptyDatabase();
+        }
 
-            PupilService pupilService = new(DatabaseProperties.NAME);
+        static async Task CreateEmptyDatabase()
+        {
+            await CreateAndInitTable<Caregiver>();
+            await CreateAndInitTable<CaregiverMap>();
+            await CreateAndInitTable<Composer>(ComposerGetter.GetFromOpenOpus());
+            await CreateAndInitTable<FixedLesson>();
+            await CreateAndInitTable<Lesson>();
+            await CreateAndInitTable<LessonRestriction>();
+            await CreateAndInitTable<LessonTime>();
+            await CreateAndInitTable<Pupil>();
+            await CreateAndInitTable<Repertoire>();
+            await CreateAndInitTable<Work>(WorkGetter.GetFromOpenOpus());
+        }
+
+        static async Task CreateAndInitTable<T>(IEnumerable<T>? data = null) where T : class, ISqlStorable, new()
+        {
+            TableConnection<T> tableConnection = new(DatabaseProperties.NAME);
+            await tableConnection.InitAsync();
+            if (data is not null)
+            {
+                await tableConnection.InsertAllAsync(data);
+            }
         }
     }
 }
