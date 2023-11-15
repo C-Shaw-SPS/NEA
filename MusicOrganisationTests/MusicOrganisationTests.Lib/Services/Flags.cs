5a64f76d9154e23eb6042163623c7703c87d652f
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-11-15 18:18:41 +0000

    flags

---
+++ MusicOrganisationTests/MusicOrganisationTests.Lib/Services/Flags.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisationTests.Lib.Services
+{
+    public static class Flags
+    {
+        public static bool HasFlagAtIndex(this uint flags, int index)
+        {
+            return ((flags >> index) & 1) == 1;
+        }
+
+        public static void AddFlagAtIndex(this ref uint flags, int index)
+        {
+            uint newFlags = 1u << index;
+            flags |= newFlags;
+        }
+
+        public static void RemoveFlagAtIndex(this ref uint flags, int index)
+        {
+            uint remainingFlags = ~(1u << index);
+            flags &= remainingFlags;
+        }
+    }
+}
