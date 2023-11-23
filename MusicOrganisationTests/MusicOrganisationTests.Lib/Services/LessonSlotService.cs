91ea31a6941794bb5b33b0a6666fa2f0a0c1bc60
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-11-23 11:32:59 +0000

    add get next flag index

--- MusicOrganisationTests/MusicOrganisationTests.Lib/Services/LessonSlotService.cs
+++ MusicOrganisationTests/MusicOrganisationTests.Lib/Services/LessonSlotService.cs
@@ -1,17 +1,34 @@
-﻿using System;
+﻿using MusicOrganisationTests.Lib.Databases;
+using MusicOrganisationTests.Lib.Tables;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MusicOrganisationTests.Lib.Exceptions;
 
 namespace MusicOrganisationTests.Lib.Services
 {
-    internal class LessonSlotService : Service
+    public class LessonSlotService : Service
     {
+        private const int _MAX_FLAG_INDEX = 32;
+
         public LessonSlotService(string path) : base(path)
         {
         }
 
-
+        public async Task<int> GetNextFlagIndexAsync(DayOfWeek dayOfWeek)
+        {
+            IEnumerable<LessonSlotData> lessonSlotData = await GetWhereEqualAsync<LessonSlotData>(nameof(LessonSlotData.DayOfWeek), dayOfWeek);
+            HashSet<int> flagIndexes = lessonSlotData.Select(data => data.FlagIndex).ToHashSet();
+            for (int newIndex = 0; newIndex < _MAX_FLAG_INDEX; ++newIndex)
+            {
+                if (!flagIndexes.Contains(newIndex))
+                {
+                    return newIndex;
+                }
+            }
+            throw new NoMoreFlagsException(dayOfWeek);
+        }
     }
 }
