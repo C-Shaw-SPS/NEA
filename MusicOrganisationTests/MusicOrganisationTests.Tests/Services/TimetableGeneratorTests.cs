572a9f53d71c838e112a3d0e2d6698552aedbae3
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-11-30 13:42:06 +0000

    Create TimetableGeneratorTests.cs

---
+++ MusicOrganisationTests/MusicOrganisationTests.Tests/Services/TimetableGeneratorTests.cs
@@ -0,0 +1,29 @@
+﻿using MusicOrganisationTests.Lib.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisationTests.Tests.Services
+{
+    public class TimetableGeneratorTests
+    {
+        List<PupilData> pupils = new()
+        {
+            new PupilData
+            {
+                Id = 0,
+                Name = "Pupil 0",
+                NeedsDifferentTimes = true,
+
+            }
+        };
+
+        [Fact]
+        public void TestTimetableGenerator()
+        {
+            
+        }
+    }
+}
