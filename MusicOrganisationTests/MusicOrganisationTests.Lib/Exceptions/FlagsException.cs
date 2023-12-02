01b23572358b659198ab497777010f79ac1096aa
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-12-02 15:57:49 +0000

    Create FlagsException.cs

---
+++ MusicOrganisationTests/MusicOrganisationTests.Lib/Exceptions/FlagsException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisationTests.Lib.Exceptions
+{
+    public class FlagsException : Exception
+    {
+        public FlagsException() : base() { }
+
+        public FlagsException(string message) : base(message) { }
+    }
+}
