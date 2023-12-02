560135adb0b1eb544a68fe7965d7897aa0d94961
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-12-02 15:58:29 +0000

    Update TimetableService.cs

--- MusicOrganisationTests/MusicOrganisationTests.Lib/Services/TimetableService.cs
+++ MusicOrganisationTests/MusicOrganisationTests.Lib/Services/TimetableService.cs
@@ -12,31 +12,7 @@ namespace MusicOrganisationTests.Lib.Services
     {
         public TimetableService(string path) : base(path)
         {
-        }
-
-        public bool IsValidLessonSlot(LessonSlotData lessonSlotData, PupilData pupilData)
-        {
-            return IsPupilAvaliable(lessonSlotData, pupilData) && IsLongEnoughTimeSlot(lessonSlotData, pupilData);
-        }
-
-        private bool IsPupilAvaliable(LessonSlotData lessonSlotData, PupilData pupilData)
-        {
-            return lessonSlotData.DayOfWeek switch
-            {
-                DayOfWeek.Monday => pupilData.MondayLessonSlots.HasFlagAtIndex(lessonSlotData.FlagIndex),
-                DayOfWeek.Tuesday => pupilData.TuesdayLessonSlots.HasFlagAtIndex(lessonSlotData.FlagIndex),
-                DayOfWeek.Wednesday => pupilData.WednesdayLessonSlots.HasFlagAtIndex(lessonSlotData.FlagIndex),
-                DayOfWeek.Thursday => pupilData.ThursdayLessonSlots.HasFlagAtIndex(lessonSlotData.FlagIndex),
-                DayOfWeek.Friday => pupilData.FridayLessonSlots.HasFlagAtIndex(lessonSlotData.FlagIndex),
-                DayOfWeek.Saturday => pupilData.SaturdayLessonSlots.HasFlagAtIndex(lessonSlotData.FlagIndex),
-                DayOfWeek.Sunday => pupilData.SundayLessonSlots.HasFlagAtIndex(lessonSlotData.FlagIndex),
-                _ => throw new DayOfWeekException(lessonSlotData.DayOfWeek),
-            };
-        }
-
-        private bool IsLongEnoughTimeSlot(LessonSlotData lessonSlotData, PupilData pupilData)
-        {
-            return (lessonSlotData.EndTime - lessonSlotData.StartTime) >= pupilData.LessonDuration;
+            throw new NotImplementedException();
         }
     }
 }
