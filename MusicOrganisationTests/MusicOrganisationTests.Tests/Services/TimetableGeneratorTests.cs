4f178daba9cfe1abf903fb4b253d51630e4e5893
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-12-02 15:58:35 +0000

    Update TimetableGeneratorTests.cs

--- MusicOrganisationTests/MusicOrganisationTests.Tests/Services/TimetableGeneratorTests.cs
+++ MusicOrganisationTests/MusicOrganisationTests.Tests/Services/TimetableGeneratorTests.cs
@@ -9,6 +9,42 @@ namespace MusicOrganisationTests.Tests.Services
 {
     public class TimetableGeneratorTests
     {
+        List<LessonSlotData> _lessonSlots = new()
+        {
+            new LessonSlotData
+            {
+                Id = 0,
+                DayOfWeek = DayOfWeek.Monday,
+                FlagIndex = 0,
+                StartTime = new TimeSpan(09,00,00),
+                EndTime = new TimeSpan(09,30,00)
+            },
+            new LessonSlotData
+            {
+                Id = 1,
+                DayOfWeek = DayOfWeek.Monday,
+                FlagIndex = 1,
+                StartTime = new TimeSpan(09,30,00),
+                EndTime = new TimeSpan(10,00,00)
+            },
+            new LessonSlotData
+            {
+                Id = 2,
+                DayOfWeek = DayOfWeek.Monday,
+                FlagIndex = 3,
+                StartTime = new TimeSpan(10,15,00),
+                EndTime = new TimeSpan(11,00,00)
+            },
+            new LessonSlotData
+            {
+                Id = 3,
+                DayOfWeek = DayOfWeek.Monday,
+                FlagIndex = 4,
+                StartTime = new TimeSpan(11,00,00),
+                EndTime = new TimeSpan(12,00,00)
+            }
+        };
+
         List<PupilData> pupils = new()
         {
             new PupilData
@@ -18,7 +54,6 @@ namespace MusicOrganisationTests.Tests.Services
                 NeedsDifferentTimes = true,
                 LessonDuration = TimeSpan.FromMinutes(30),
                 MondayLessonSlots = 0b1001,
-
             }
         };
 
