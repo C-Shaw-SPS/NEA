9af27c188da9d92c7200844b73dedb9b94307a55
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-12-02 15:58:14 +0000

    add get number of flags and get index of first flag

--- MusicOrganisationTests/MusicOrganisationTests.Lib/Services/Flags.cs
+++ MusicOrganisationTests/MusicOrganisationTests.Lib/Services/Flags.cs
@@ -1,28 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MusicOrganisationTests.Lib.Exceptions;
 
 namespace MusicOrganisationTests.Lib.Services
 {
     public static class Flags
     {
+        public const int MAX_FLAG_INDEX = 32;
+
         public static bool HasFlagAtIndex(this int flags, int index)
         {
             return ((flags >> index) & 1) == 1;
         }
 
-        public static void AddFlagAtIndex(this ref int flags, int index)
+        /// <summary>
+        /// Adds a flag at the specified index
+        /// </summary>
+        /// <returns>
+        /// True if a new flag was added at the index, Flase if there was already a flag at the index
+        /// </returns>
+        public static bool AddFlagAtIndex(this ref int flags, int index)
         {
+            bool hadFlagAtIndex = HasFlagAtIndex(flags, index);
             int newFlags = 1 << index;
             flags |= newFlags;
+            return hadFlagAtIndex;
         }
 
-        public static void RemoveFlagAtIndex(this ref int flags, int index)
+        /// <summary>
+        /// Removes a flag at the specified index
+        /// </summary>
+        /// <returns>
+        /// True if an existing flag was removed at the index, False if there was no flag at the index
+        /// </returns>
+        public static bool RemoveFlagAtIndex(this ref int flags, int index)
         {
+            bool hadFlagAtIndex = HasFlagAtIndex(flags, index);
             int remainingFlags = ~(1 << index);
             flags &= remainingFlags;
+            return hadFlagAtIndex;
+        }
+
+        public static int GetNumberOfFlags(this int flags)
+        {
+            int count = 0;
+            while (flags != 0)
+            {
+                if ((flags & 1) == 1)
+                {
+                    count++;
+                }
+                flags >>= 1;
+            }
+            return count;
+        }
+
+        public static int GetIndexOfFirstFlag(this int flags)
+        {
+            for (int i = 0; i < MAX_FLAG_INDEX; ++i)
+            {
+                if (flags.HasFlagAtIndex(i))
+                {
+                    return i;
+                }
+            }
+
+            throw new FlagsException("No flags set to true");
         }
     }
 }
