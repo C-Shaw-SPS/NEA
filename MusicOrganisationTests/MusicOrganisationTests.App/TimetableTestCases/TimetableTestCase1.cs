8cd10ce6aa432c649d1d741f2db46023e7941f3c
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2023-12-06 18:10:57 +0000

    Update TimetableTestCase1.cs

--- MusicOrganisationTests/MusicOrganisationTests.App/TimetableTestCases/TimetableTestCase1.cs
+++ MusicOrganisationTests/MusicOrganisationTests.App/TimetableTestCases/TimetableTestCase1.cs
@@ -1,11 +1,6 @@
 ﻿using MusicOrganisationTests.Lib.Models;
 using MusicOrganisationTests.Lib.Services;
 using MusicOrganisationTests.Lib.Tables;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MusicOrganisationTests.App.TimetableTestCases
 {
