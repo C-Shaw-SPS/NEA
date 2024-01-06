fe5d9f2ef08aea6f73c9012a58afa30034740b7f
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-06 13:04:31 +0000

    add database stuff

---
+++ MusicOrganisation/MusicOrganisation.Lib/Databases/DatabaseProperties.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace MusicOrganisation.Lib.Databases
+{
+    public static class DatabaseProperties
+    {
+        public const string NAME = "MusicOrganisation.db";
+
+        internal const SQLiteOpenFlags FLAGS =
+            SQLiteOpenFlags.ReadWrite |
+            SQLiteOpenFlags.Create |
+            SQLiteOpenFlags.SharedCache;
+    }
+}
---
+++ MusicOrganisation/MusicOrganisation.Lib/Databases/ITable.cs
@@ -0,0 +1,13 @@
+﻿namespace MusicOrganisation.Lib.Databases
+{
+    public interface ITable
+    {
+        public abstract int Id { get; }
+
+        public abstract static string TableName { get; }
+
+        public IEnumerable<string> GetSqlValues();
+
+        public abstract static IEnumerable<string> GetColumnNames();
+    }
+}
