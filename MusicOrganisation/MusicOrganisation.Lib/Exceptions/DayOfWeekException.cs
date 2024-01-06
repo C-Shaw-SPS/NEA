716ccaf0517b1bab7e7d7eb393252b5488ad7404
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-06 13:08:54 +0000

    add exceptions

---
+++ MusicOrganisation/MusicOrganisation.Lib/Exceptions/DayOfWeekException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisation.Lib.Exceptions
+{
+    public class DayOfWeekException : Exception
+    {
+        public DayOfWeekException(DayOfWeek dayOfWeek) : base($"No such day of week '{dayOfWeek}'") { }
+    }
+}
