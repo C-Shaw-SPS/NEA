d1b1911082c3b26925c0f525c60a1436055afda0
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-16 15:36:13 +0000

    Create ModelViewModelBase.cs

---
+++ MusicOrganisation/MusicOrganisation.Lib/ViewModels/ModelViewModels/ModelViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using MusicOrganisation.Lib.Viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisation.Lib.ViewModels.ModelViewModels
+{
+    public class ModelViewModelBase<T> : ViewModelBase
+    {
+        public const string ID_PARAMETER = nameof(ID_PARAMETER);
+    }
+}
