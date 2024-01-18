0bdcad0f1076cac0caf4c0cb84e88ac041bafd42
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-18 15:00:10 +0000

    Create IModel.cs

---
+++ MusicOrganisation/MusicOrganisation.Lib/Models/IModel.cs
@@ -0,0 +1,13 @@
+﻿using MusicOrganisation.Lib.Databases;
+
+namespace MusicOrganisation.Lib.Models
+{
+    public interface IModel
+    {
+        public ISqlStatement GetSelectAllQuery();
+
+        public ISqlStatement GetUpdateQuery();
+
+        public ISqlStatement GetDeleteQuery();
+    }
+}
