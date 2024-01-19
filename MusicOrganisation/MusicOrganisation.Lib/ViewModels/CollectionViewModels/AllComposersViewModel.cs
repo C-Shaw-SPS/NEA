6b6365f2a21ff319e9afc5bb7e68158b2470a9b2
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-19 15:37:46 +0000

    delete viewmodels

--- MusicOrganisation/MusicOrganisation.Lib/ViewModels/CollectionViewModels/AllComposersViewModel.cs
+++ MusicOrganisation/MusicOrganisation.Lib/ViewModels/CollectionViewModels/AllComposersViewModel.cs
@@ -1,25 +1,62 @@
-﻿using MusicOrganisation.Lib.Tables;
-using MusicOrganisation.Lib.ViewModels.EditViewModels;
-using MusicOrganisation.Lib.ViewModels.ModelViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MusicOrganisation.Lib.Databases;
+using MusicOrganisation.Lib.Tables;
+using MusicOrganisation.Lib.Viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MusicOrganisation.Lib.ViewModels.CollectionViewModels
 {
-    public partial class AllComposersViewModel : CollectionViewModelBase<ComposerData, ComposerViewModel, EditComposerViewModel>, IViewModel
+    public partial class AllComposersViewModel : ViewModelBase
     {
-        private const string _ROUTE = nameof(AllComposersViewModel);
-
-        private static readonly Dictionary<string, string> _orderings = new()
+        private static Dictionary<string, string> _orderings = new()
         {
-            { "Name", nameof(ComposerData.Name) },
-            { "Year of birth", nameof(ComposerData.BirthYear) },
-            { "Year of death", nameof(ComposerData.DeathYear) }
+            ["Name"] = nameof(ComposerData.Name),
+            ["Year of birth"] = nameof(ComposerData.BirthYear),
+            ["Year of death"] = nameof(ComposerData.DeathYear)
         };
 
-        public AllComposersViewModel() : base(_orderings, nameof(ComposerData.Name))
-        {
+        [ObservableProperty]
+        private string _searchText;
 
+        [ObservableProperty]
+        private string _selectedOrdering;
+
+        [ObservableProperty]
+        private ComposerData? _selectedItem;
+
+        [ObservableProperty]
+        private ObservableCollection<ComposerData> _collection;
+
+        private AsyncRelayCommand _searchCommand;
+        private AsyncRelayCommand _selectCommand;
+        private AsyncRelayCommand _newCommand;
+
+        public AllComposersViewModel()
+        {
+            _collection = [];
+            _searchCommand = new(SearchAsync);
         }
 
-        public static string Route => _ROUTE;
+        public AsyncRelayCommand SearchCommand => _searchCommand;
+
+        public AsyncRelayCommand SelectCommand => _selectCommand;
+
+        public AsyncRelayCommand NewCommand => _newCommand;
+
+        public async Task SearchAsync()
+        {
+            string ordering = _orderings[SelectedOrdering];
+            SqlQuery<ComposerData> sqlQuery = new(SqlQuery.DEFAULT_LIMIT);
+            sqlQuery.SetSelectAll();
+            sqlQuery.AddWhereLike<ComposerData>(nameof(ComposerData.Name), SearchText);
+            sqlQuery.AddOrderBy<ComposerData>(ordering);
+            throw new NotImplementedException();
+        }
     }
 }
