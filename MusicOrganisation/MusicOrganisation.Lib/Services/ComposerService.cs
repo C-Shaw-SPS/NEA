f53249997f7ae54afaa5ad9e6d94906043a16d65
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-21 15:44:30 +0000

    Create ComposerService.cs

---
+++ MusicOrganisation/MusicOrganisation.Lib/Services/ComposerService.cs
@@ -0,0 +1,38 @@
+﻿using MusicOrganisation.Lib.Databases;
+using MusicOrganisation.Lib.Json;
+using MusicOrganisation.Lib.Tables;
+
+namespace MusicOrganisation.Lib.Services
+{
+    public class ComposerService : IService<ComposerData>
+    {
+        private readonly Database _database;
+
+        public ComposerService(Database database)
+        {
+            _database = database;
+        }
+
+        public async Task DeleteAsync(ComposerData value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ComposerData>> GetAllAsync()
+        {
+            IEnumerable<ComposerData> composers = await _database.GetAllAsync<ComposerData>();
+            return composers;
+        }
+
+        public async Task UpdateAsync(ComposerData value)
+        {
+            await _database.UpdateAsync(value);
+        }
+
+        public async Task InitialiseData()
+        {
+            IEnumerable<ComposerData> composers = await ComposerGetter.GetFromOpenOpus();
+            await _database.InsertAllAsync(composers);
+        }
+    }
+}
