ad43f7aed42594e6826b9ddb816e112db10be868
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-21 17:11:23 +0000

    Create Composer.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/Tables/Composer.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+
+namespace MusicOrganisationApp.Lib.Tables
+{
+    [Table(_TABLE_NAME)]
+    public class Composer : ITable
+    {
+        private const string _TABLE_NAME = $"{nameof(Composer)}s";
+
+        public static string TableName => _TABLE_NAME;
+
+
+        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public static IEnumerable<string> GetColumnNames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, string> GetSqlValues()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
