7af16db7f0989e4d41e92b0200155f5633d9fc2f
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-21 15:44:35 +0000

    Create IService.cs

---
+++ MusicOrganisation/MusicOrganisation.Lib/Services/IService.cs
@@ -0,0 +1,18 @@
+﻿using MusicOrganisation.Lib.Databases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisation.Lib.Services
+{
+    public interface IService<T>
+    {
+        public Task<IEnumerable<T>> GetAllAsync();
+
+        public Task UpdateAsync(T value);
+
+        public Task DeleteAsync(T value);
+    }
+}
