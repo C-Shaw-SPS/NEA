cf89dbd76299bbaa4754f968038d0e5920207fa3
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-21 17:54:39 +0000

    Create DropTableStatement.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/Databases/DropTableStatement.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicOrganisationApp.Lib.Databases
+{
+    public class DropTableStatement<T> : ISqlStatement where T : class, ITable, new()
+    {
+        private readonly string _sql = $"DROP TABLE {T.TableName}";
+
+        public string GetSql()
+        {
+            return _sql;
+        }
+    }
+}
