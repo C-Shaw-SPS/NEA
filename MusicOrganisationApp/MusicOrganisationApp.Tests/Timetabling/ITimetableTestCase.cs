42327b14cd856c73315d6a780291499866bfa349
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-21 18:26:17 +0000

    timetabling and tests

---
+++ MusicOrganisationApp/MusicOrganisationApp.Tests/Timetabling/ITimetableTestCase.cs
@@ -0,0 +1,18 @@
+﻿using MusicOrganisationApp.Lib.Models;
+using MusicOrganisationApp.Lib.Tables;
+
+namespace MusicOrganisationApp.Tests.Timetabling
+{
+    public interface ITimetableTestCase
+    {
+        public abstract static bool IsPossible { get; }
+
+        public abstract static IEnumerable<Pupil> Pupils { get; }
+
+        public abstract static IEnumerable<LessonSlotData> LessonSlots { get; }
+
+        public abstract static IEnumerable<LessonData> PrevLessons { get; }
+
+        public abstract static Dictionary<int, int>? ExpectedTimetable { get; }
+    }
+}
