445e25edff65e9ce63aa4a6c41f84b6fc0d18841
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-21 17:02:13 +0000

    Create DeleteStatement.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/Databases/DeleteStatement.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace MusicOrganisationApp.Lib.Databases
+{
+    public class DeleteStatement<T> where T : class, ITable, new()
+    {
+        private readonly List<(string column, string value)> _conditions;
+
+        public DeleteStatement()
+        {
+            _conditions = [];
+        }
+
+        public string TableName => T.TableName;
+
+        public void AddCondition(string column, object? value)
+        {
+            _conditions.Add((column, value.FormatSqlValue()));
+        }
+
+        public string GetSql()
+        {
+            StringBuilder stringBuilder = new();
+            stringBuilder.AppendLine($"DELETE FROM {T.TableName} WHERE");
+            stringBuilder.AppendLine(GetConditions());
+            string sql = stringBuilder.ToString();
+            return sql;
+        }
+
+        private string GetConditions()
+        {
+            IEnumerable<string> conditions = _conditions.Select(condition => FormatCondition(condition.column, condition.value));
+            return string.Join("\nOR", conditions);
+        }
+
+        private static string FormatCondition(string column, string value)
+        {
+            return $"{column} = {value}";
+        }
+    }
+}
