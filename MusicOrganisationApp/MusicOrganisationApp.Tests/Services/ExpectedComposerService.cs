cfa4e330b3565aadd4800c76acd0b631aa16cad3
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-22 13:31:54 +0000

    Create ExpectedComposerService.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Tests/Services/ExpectedComposerService.cs
@@ -0,0 +1,21 @@
+﻿using MusicOrganisationApp.Lib.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisationApp.Tests.Services
+{
+    internal static class ExpectedComposerService
+    {
+        public static readonly List<ComposerData> Composers = new()
+        {
+            new ComposerData
+            {
+                Id = 0,
+
+            }
+        };
+    }
+}
