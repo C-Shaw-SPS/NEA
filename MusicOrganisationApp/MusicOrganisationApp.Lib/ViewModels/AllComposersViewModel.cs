df42391b7219493e85c01bcbaaae62c9f7eb7591
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-22 17:00:35 +0000

    Create AllComposersViewModel.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/AllComposersViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MusicOrganisationApp.Lib.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisationApp.Lib.ViewModels
+{
+    public partial class AllComposersViewModel : ViewModelBase
+    {
+        private readonly ComposerService _service;
+
+        [ObservableProperty]
+        private string _searchText = string.Empty;
+
+        public AllComposersViewModel()
+        {
+            _service = new(_database);
+        }
+    }
+}
