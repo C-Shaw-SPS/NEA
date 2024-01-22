44ff1e94b26aa153d59bad0f776af97c473002c5
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-22 17:00:38 +0000

    Create ViewModelBase.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MusicOrganisationApp.Lib.Databases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisationApp.Lib.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+        private readonly string _path;
+        protected readonly DatabaseConnection _database;
+
+        public ViewModelBase()
+        {
+            _path = Path.Combine(FileSystem.AppDataDirectory, DatabaseProperties.NAME);
+            _database = new(_path);
+        }
+    }
+}
