94bfa3a9305175381376e7744ff657bcfbe077b2
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-22 13:58:41 +0000

    Update ExpectedComposerService.cs

--- MusicOrganisationApp/MusicOrganisationApp.Tests/Services/ExpectedComposerService.cs
+++ MusicOrganisationApp/MusicOrganisationApp.Tests/Services/ExpectedComposerService.cs
@@ -1,9 +1,4 @@
 ﻿using MusicOrganisationApp.Lib.Tables;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MusicOrganisationApp.Tests.Services
 {
@@ -13,8 +8,59 @@ namespace MusicOrganisationApp.Tests.Services
         {
             new ComposerData
             {
-                Id = 0,
+                Id = 0
+            },
+            new ComposerData
+            {
+                Id = 1
+            }
+        };
 
+        public static readonly List<WorkData> Works = new()
+        {
+            new WorkData
+            {
+                Id = 0,
+                ComposerId = Composers[0].Id
+            },
+            new WorkData
+            {
+                Id = 1,
+                ComposerId = Composers[0].Id
+            },
+            new WorkData
+            {
+                Id = 2,
+                ComposerId = Composers[1].Id
+            },
+            new WorkData
+            {
+                Id = 3,
+                ComposerId = Composers[1].Id
+            }
+        };
+
+        public static readonly List<RepertoireData> Repertoires = new()
+        {
+            new RepertoireData
+            {
+                Id = 0,
+                WorkId = Works[0].Id
+            },
+            new RepertoireData
+            {
+                Id = 1,
+                WorkId = Works[1].Id
+            },
+            new RepertoireData
+            {
+                Id = 2,
+                WorkId = Works[2].Id
+            },
+            new RepertoireData
+            {
+                Id = 3,
+                WorkId = Works[3].Id
             }
         };
     }
