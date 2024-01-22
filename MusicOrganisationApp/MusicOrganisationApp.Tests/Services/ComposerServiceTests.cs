a508f8019ffbc5f040fec10a4fbce6ac44f67ec2
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-22 13:58:36 +0000

    Update ComposerServiceTests.cs

--- MusicOrganisationApp/MusicOrganisationApp.Tests/Services/ComposerServiceTests.cs
+++ MusicOrganisationApp/MusicOrganisationApp.Tests/Services/ComposerServiceTests.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualStudio.TestPlatform.PlatformAbstractions;
+using MusicOrganisationApp.Lib.Databases;
+using MusicOrganisationApp.Lib.Services;
+using MusicOrganisationApp.Lib.Tables;
 
 namespace MusicOrganisationApp.Tests.Services
 {
     public class ComposerServiceTests
     {
+        [Fact]
+        public async Task TestAddComposerAsync()
+        {
+            DatabaseConnection database = new(nameof(TestAddComposerAsync));
+            await database.DropTableAsync<ComposerData>();
+
+            ComposerService composerService = new(database);
+            await composerService.InsertAsync(ExpectedComposerService.Composers[0]);
+
+            IEnumerable<ComposerData> actualComposers = await composerService.GetAllAsync();
+
+            Assert.Single(actualComposers);
+            Assert.Contains(ExpectedComposerService.Composers[0], actualComposers);
+        }
+
+        [Fact]
+        public async Task TestDeleteComposerAsync()
+        {
+            DatabaseConnection database = new(nameof(TestAddComposerAsync));
+            await database.DropTableAsync<ComposerData>();
+            await database.DropTableAsync<WorkData>();
+            await database.DropTableAsync<RepertoireData>();
+
+            await database.InsertAllAsync(ExpectedComposerService.Composers);
+            await database.InsertAllAsync(ExpectedComposerService.Works);
+            await database.InsertAllAsync(ExpectedComposerService.Repertoires);
+
+            ComposerData composerToDelete = ExpectedComposerService.Composers[0];
+            IEnumerable<WorkData> worksToDelete = ExpectedComposerService.Works.Where(work => work.ComposerId == composerToDelete.Id);
+            IEnumerable<RepertoireData> repertoireToDelete = ExpectedComposerService.Repertoires.Where(repetroire => worksToDelete.Any(work => work.Id == repetroire.WorkId));
+
+            ComposerService composerService = new(database);
+            await composerService.DeleteAsync(composerToDelete);
+
+            IEnumerable<ComposerData> remainingComposers = await database.GetAllAsync<ComposerData>();
+            IEnumerable<WorkData> remainingWorks = await database.GetAllAsync<WorkData>();
+            IEnumerable<RepertoireData> remainingRepertoire = await database.GetAllAsync<RepertoireData>();
+
+            Assert.DoesNotContain(composerToDelete, remainingComposers);
+            foreach (WorkData deletedWork in worksToDelete)
+            {
+                Assert.DoesNotContain(deletedWork, remainingWorks);
+            }
+            foreach (RepertoireData deletedRepertoire in repertoireToDelete)
+            {
+                Assert.DoesNotContain(deletedRepertoire, remainingRepertoire);
+            }
+        }
     }
 }
