382b05da00723921a68c78803f06caf8c58154aa
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-23 16:17:05 +0000

    navigation

--- MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/ViewModelBase.cs
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/ViewModelBase.cs
@@ -10,6 +10,8 @@ namespace MusicOrganisationApp.Lib.ViewModels
 {
     public abstract class ViewModelBase : ObservableObject
     {
+        protected const string _RETURN = "..";
+
         private readonly string _path;
         protected readonly DatabaseConnection _database;
 
@@ -17,6 +19,33 @@ namespace MusicOrganisationApp.Lib.ViewModels
         {
             _path = Path.Combine(FileSystem.AppDataDirectory, DatabaseProperties.NAME);
             _database = new(_path);
+        }
+
+        protected static async Task GoToAsync(Dictionary<string, object?> parameters, params string[] routes)
+        {
+            string route = GetRoute(routes);
+            await Shell.Current.GoToAsync(route, parameters);
+        }
+
+        protected static async Task ReturnAsync(Dictionary<string, object?> parameters)
+        {
+            await GoToAsync(parameters, _RETURN);
+        }
+
+        protected static async Task GoToAsync(params string[] routes)
+        {
+            string route = GetRoute(routes);
+            await Shell.Current.GoToAsync(route);
+        }
+
+        protected static async Task ReturnAsync()
+        {
+            await GoToAsync(_RETURN);
+        }
+
+        private static string GetRoute(IEnumerable<string> routes)
+        {
+            return string.Join('/', routes);
         }
     }
 }
