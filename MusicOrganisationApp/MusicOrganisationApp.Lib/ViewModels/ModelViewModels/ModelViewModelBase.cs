21d6e27ebb55a7f42366b5275176aa73a8c8ec2a
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-25 20:51:55 +0000

    Create ModelViewModelBase.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/ModelViewModels/ModelViewModelBase.cs
@@ -0,0 +1,58 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using MusicOrganisationApp.Lib.Models;
+using MusicOrganisationApp.Lib.Services;
+
+namespace MusicOrganisationApp.Lib.ViewModels.ModelViewModels
+{
+    public abstract class ModelViewModelBase : ViewModelBase
+    {
+        public const string ID_PARAMETER = nameof(ID_PARAMETER);
+    }
+
+    public abstract class ModelViewModelBase<T> : ModelViewModelBase, IQueryAttributable where T : class, IModel, new()
+    {
+        private readonly string _editPath;
+        private readonly AsyncRelayCommand _editCommand;
+        private T _value;
+
+        public ModelViewModelBase(string editPath)
+        {
+            _editCommand = new(EditAsync);
+            _editPath = editPath;
+            _value = new();
+        }
+
+        protected abstract IService<T> Service { get; } 
+
+        public AsyncRelayCommand EditCommand => _editCommand;
+
+        private async Task EditAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.TryGetValue(ID_PARAMETER, out object? value) && value is int id)
+            {
+                await SetValueAsync(id);
+            }
+        }
+
+        private async Task SetValueAsync(int id)
+        {
+            (bool suceeded, T value) = await Service.TryGetAsync(id);
+            if (suceeded)
+            {
+                _value = value;
+                SetDisplayValues();
+            }
+            else
+            {
+                await GoBackAsync();
+            }
+        }
+
+        protected abstract void SetDisplayValues();
+    }
+}
