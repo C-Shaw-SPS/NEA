bbe0a42710f50587aa2f6dfd27cf9af2fd7a1b13
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-25 13:50:20 +0000

    Create AllWorksViewModel.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/CollectionViewModels/AllWorksViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MusicOrganisationApp.Lib.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisationApp.Lib.ViewModels.CollectionViewModels
+{
+    public class AllWorksViewModel : ViewModelBase
+    {
+        private readonly WorkService _service;
+
+        public AllWorksViewModel()
+        {
+            _service = new(_database);
+        }
+    }
+}
