41a666b4a9ca2cf7f730fc4913f63861bccd6589
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-25 20:51:53 +0000

    Create EditViewModelBase.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/EditViewModels/EditViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using MusicOrganisationApp.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisationApp.Lib.ViewModels.EditViewModels
+{
+    public class EditViewModelBase : ViewModelBase
+    {
+        public const string ID_PARAMETER = nameof(ID_PARAMETER);
+        public const string IS_NEW_PARAMETER = nameof(IS_NEW_PARAMETER);
+    }
+
+    public class EditViewModelBase<T> : EditViewModelBase where T : class, IModel, new()
+    {
+
+    }
+}
