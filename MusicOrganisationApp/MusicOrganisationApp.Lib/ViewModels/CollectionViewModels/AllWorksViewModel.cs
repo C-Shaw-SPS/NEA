e5d1c8ef3a962b5f9c28fa7109631168106d643e
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-26 14:49:56 +0000

    inherit from collection base

--- MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/CollectionViewModels/AllWorksViewModel.cs
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/CollectionViewModels/AllWorksViewModel.cs
@@ -1,12 +1,11 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using MusicOrganisationApp.Lib.Models;
+﻿using MusicOrganisationApp.Lib.Models;
 using MusicOrganisationApp.Lib.Services;
-using System.Collections.ObjectModel;
+using MusicOrganisationApp.Lib.ViewModels.EditViewModels;
+using MusicOrganisationApp.Lib.ViewModels.ModelViewModels;
 
 namespace MusicOrganisationApp.Lib.ViewModels.CollectionViewModels
 {
-    public partial class AllWorksViewModel : ViewModelBase
+    public partial class AllWorksViewModel : CollectionViewModelBase<Work>
     {
         private const string _ROUTE = nameof(AllWorksViewModel);
 
@@ -17,62 +16,14 @@ namespace MusicOrganisationApp.Lib.ViewModels.CollectionViewModels
         };
 
         private readonly WorkService _service;
-        private readonly AsyncRelayCommand _addNewCommand;
-        private readonly AsyncRelayCommand _searchCommand;
-        private readonly AsyncRelayCommand _selectCommand;
 
-        [ObservableProperty]
-        private string _searchText;
-
-        [ObservableProperty]
-        private string _selectedOrdering;
-
-        [ObservableProperty]
-        private ObservableCollection<Work> _collection;
-
-        [ObservableProperty]
-        private Work? _selectedWork;
-
-        public AllWorksViewModel()
+        public AllWorksViewModel() : base(WorkViewModel.Route, EditWorkViewModel.Route, _orderings)
         {
             _service = new(_database);
-            _addNewCommand = new(AddNewAsync);
-            _searchCommand = new(SearchAsync);
-            _selectCommand = new(SelectAsync);
-
-            _searchText = string.Empty;
-            _selectedOrdering = _orderings.Keys.First();
-            _collection = [];
         }
 
         public static string Route => _ROUTE;
 
-        public static List<string> Orderings => _orderings.Keys.ToList();
-
-        public AsyncRelayCommand AddNewCommand => _addNewCommand;
-
-        public AsyncRelayCommand SearchCommand => _searchCommand;
-
-        public AsyncRelayCommand SelectCommand => _selectCommand;
-
-        public async Task RefreshAsync()
-        {
-            await SearchAsync();
-        }
-
-        private async Task AddNewAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        private async Task SearchAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        private async Task SelectAsync()
-        {
-            throw new NotImplementedException();
-        }
+        protected override IService<Work> Service => _service;
     }
 }
