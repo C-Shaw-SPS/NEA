cf00e0f7e5a9e72ab73a0489ea2e0f342b94de0d
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-30 13:51:09 +0000

    Create AllPupilsViewModel.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/CollectionViewModels/AllPupilsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MusicOrganisationApp.Lib.Models;
+using MusicOrganisationApp.Lib.Services;
+using MusicOrganisationApp.Lib.ViewModels.EditViewModels;
+using MusicOrganisationApp.Lib.ViewModels.ModelViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOrganisationApp.Lib.ViewModels.CollectionViewModels
+{
+    public class AllPupilsViewModel : CollectionViewModelBase<Pupil>
+    {
+        private static Dictionary<string, string> _orderings = new()
+        {
+            ["Name"] = nameof(Pupil.Name)
+        };
+
+        public AllPupilsViewModel() : base(PupilViewModel.Route, EditPupilViewModel.Route, _orderings)
+        {
+        }
+
+        protected override IService<Pupil> Service => throw new NotImplementedException();
+    }
+}
