fc56e9b08d3dcda0714c1b7b8ee3e75f84fe0768
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-30 14:27:57 +0000

    validate lesson duration

--- MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/EditViewModels/EditPupilViewModel.cs
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/EditViewModels/EditPupilViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using MusicOrganisationApp.Lib.Models;
 using MusicOrganisationApp.Lib.Services;
+using SQLitePCL;
 
 namespace MusicOrganisationApp.Lib.ViewModels.EditViewModels
 {
@@ -12,7 +13,7 @@ namespace MusicOrganisationApp.Lib.ViewModels.EditViewModels
         private const string _NEW_PAGE_TITLE = "New pupil";
 
         private const string _BLANK_NAME_ERROR = "Name cannot be blank";
-        private const string _NEGATIVE_LESSON_DURATION_ERROR = "Lesson duration cannot be negative";
+        private const string _INVALID_DURATION_ERROR = "Invalid lesson duration";
 
         private readonly PupilService _service;
 
@@ -26,7 +27,10 @@ namespace MusicOrganisationApp.Lib.ViewModels.EditViewModels
         private bool _needsDifferentTimes = false;
 
         [ObservableProperty]
-        private TimeSpan _lessonDuration;
+        private string _lessonDurationHours = string.Empty;
+
+        [ObservableProperty]
+        private string _lessonDurationMinutes = string.Empty;
 
         [ObservableProperty]
         private string _email = string.Empty;
@@ -38,7 +42,7 @@ namespace MusicOrganisationApp.Lib.ViewModels.EditViewModels
         private string _notes = string.Empty;
 
         [ObservableProperty]
-        public string _nameError = string.Empty;
+        private string _nameError = string.Empty;
 
         [ObservableProperty]
         private string _lessonDurationError = string.Empty;
@@ -57,7 +61,8 @@ namespace MusicOrganisationApp.Lib.ViewModels.EditViewModels
             Name = _value.Name;
             Level = _value.Level;
             NeedsDifferentTimes = _value.NeedsDifferentTimes;
-            LessonDuration = _value.LessonDuration;
+            LessonDurationHours = _value.LessonDuration.Hours.ToString();
+            LessonDurationMinutes = _value.LessonDuration.Minutes.ToString();
             Email = _value.Email;
             PhoneNumber = _value.PhoneNumber;
             Notes = _value.Notes;
@@ -95,16 +100,24 @@ namespace MusicOrganisationApp.Lib.ViewModels.EditViewModels
 
         private bool TrySetLessonDuration()
         {
-            if (LessonDuration < TimeSpan.Zero)
+            bool validHours = TryGetPositiveInt(LessonDurationHours, out int hours);
+            bool validMinutes = TryGetPositiveInt(LessonDurationHours, out int minutes);
+
+            if (validHours && validMinutes)
             {
-                LessonDurationError = _NEGATIVE_LESSON_DURATION_ERROR;
-                return false;
+                TimeSpan lessonDuration = new(hours, minutes, 0);
+                _value.LessonDuration = lessonDuration;
+                LessonDurationHours = lessonDuration.Hours.ToString();
+                LessonDurationMinutes = lessonDuration.Minutes.ToString();
+                LessonDurationError = string.Empty;
+                return true;
             }
             else
             {
-                _value.LessonDuration = LessonDuration;
-                LessonDurationError = string.Empty;
-                return true;
+                LessonDurationHours = string.Empty;
+                LessonDurationMinutes = string.Empty;
+                LessonDurationError = _INVALID_DURATION_ERROR;
+                return false;
             }
         }
     }
