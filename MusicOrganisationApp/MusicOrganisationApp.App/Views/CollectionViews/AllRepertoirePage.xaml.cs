54c9e25e22920c2156ea9c8581140f2f59fd11a1
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-01-31 12:36:43 +0000

    Create AllRepertoirePage.xaml.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.App/Views/CollectionViews/AllRepertoirePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MusicOrganisationApp.App.Views.CollectionViews;
+
+public partial class AllRepertoirePage : ContentPage
+{
+	public AllRepertoirePage()
+	{
+		InitializeComponent();
+	}
+}
