6222c428c47530cdaf125674405f2fccaa98b9dc
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-02-01 21:59:14 +0000

    Create InsertCommand.cs

---
+++ MusicOrganisation/MusicOrganisation.Lib/Databases/InsertCommand.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace MusicOrganisation.Lib.Databases
+{
+    internal class InsertCommand<T> where T : ITable, new()
+    {
+        private readonly StringBuilder _stringBuilder;
+        private readonly IEnumerable<string> _columns;
+        private bool _containsValues;
+
+        public InsertCommand()
+        {
+            _stringBuilder = new();
+            _columns = T.GetColumnNames();
+            _containsValues = false;
+            AddInsertLine();
+        }
+
+        private void AddInsertLine()
+        {
+            _stringBuilder.AppendLine($"INSERT INTO {T.TableName} {_columns.CommaJoin().AddBrackets()} VALUES");
+        }
+
+        public void AddValue(T value)
+        {
+            if (_containsValues)
+            {
+                _stringBuilder.Append(SqlFormatting.COMMA_SEPARATOR);
+            }
+            else
+            {
+                _containsValues = true;
+            }
+            _stringBuilder.Append(value.GetSqlValues().CommaJoin().AddBrackets());
+        }
+
+        public override string ToString()
+        {
+            return _stringBuilder.ToString();
+        }
+    }
+}
