089c652e335658b6eb9b48d2a105f12bfbf98b90
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-02-02 10:13:23 +0000

    reorder and remove nullable strings

--- MusicOrganisationApp/MusicOrganisationApp.Lib/Models/Caregiver.cs
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/Models/Caregiver.cs
@@ -2,18 +2,12 @@
 {
     public class Caregiver : IEquatable<Caregiver>, IIdentifiable
     {
-        private int _mapId;
         private int _id;
         private string _name = string.Empty;
+        private string _email = string.Empty;
+        private string _phoneNumber = string.Empty; 
+        private int _mapId;
         private string _description = string.Empty;
-        private string? _email;
-        private string? _phoneNumber;
-
-        public int MapId
-        {
-            get => _mapId;
-            set => _mapId = value;
-        }
 
         public int Id
         {
@@ -27,33 +21,39 @@
             set => _name = value;
         }
 
+        public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set => _phoneNumber = value;
+        }
+
+        public int MapId
+        {
+            get => _mapId;
+            set => _mapId = value;
+        }
+
         public string Description
         {
             get => _description;
             set => _description = value;
         }
 
-        public string? Email
-        {
-            get => _email;
-            set => _email = value;
-        }
-
-        public string? PhoneNumber
-        {
-            get => _phoneNumber;
-            set => _phoneNumber = value;
-        }
-
         public bool Equals(Caregiver? other)
         {
             return other != null
-                && _mapId == other._mapId
                 && _id == other._id
                 && _name == other._name
-                && _description == other._description
                 && _email == other._email
-                && _phoneNumber == other._phoneNumber;
+                && _phoneNumber == other._phoneNumber
+                && _mapId == other._mapId
+                && _description == other._description;
         }
     }
 }
