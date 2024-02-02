2939961718537e9ef183523861b4a370099f12ae
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-02-02 20:40:38 +0000

    Create EditPupilCaregiverViewModel.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/EditViewModels/EditPupilCaregiverViewModel.cs
@@ -0,0 +1,30 @@
+﻿using MusicOrganisationApp.Lib.Models;
+using MusicOrganisationApp.Lib.Services;
+
+namespace MusicOrganisationApp.Lib.ViewModels.EditViewModels
+{
+    public partial class EditPupilCaregiverViewModel : EditViewModelBase<PupilCaregiver>
+    {
+        private const string _EDIT_PAGE_TITLE = "Edit caregiver";
+        private const string _NEW_PAGE_TITLE = "New caregiver";
+
+        private readonly PupilCaregiverService _service;
+
+        public EditPupilCaregiverViewModel() : base(_EDIT_PAGE_TITLE, _NEW_PAGE_TITLE)
+        {
+            _service = new(_database);
+        }
+
+        protected override IService<PupilCaregiver> Service => _service;
+
+        protected override void SetDisplayValues()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool TrySetValuesToSave()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
