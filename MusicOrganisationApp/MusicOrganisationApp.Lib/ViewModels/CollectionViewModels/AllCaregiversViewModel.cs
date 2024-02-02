34264b98209403ff24f8a5abfca148ce2a9b6e6b
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-02-02 20:21:46 +0000

    Create AllCaregiversViewModel.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/CollectionViewModels/AllCaregiversViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MusicOrganisationApp.Lib.Services;
+using MusicOrganisationApp.Lib.Tables;
+using MusicOrganisationApp.Lib.ViewModels.EditViewModels;
+using MusicOrganisationApp.Lib.ViewModels.ModelViewModels;
+
+namespace MusicOrganisationApp.Lib.ViewModels.CollectionViewModels
+{
+    public partial class AllCaregiversViewModel : CollectionViewModelBase<CaregiverData>
+    {
+        private static Dictionary<string, string> _orderings = new()
+        {
+            ["Name"] = nameof(CaregiverData.Name)
+        };
+
+        private readonly CaregiverService _service;
+
+        public AllCaregiversViewModel() : base(CaregiverViewModel.Route, EditCaregiverViewModel.Route, _orderings)
+        {
+            _service = new(_database);
+        }
+
+        protected override IService<CaregiverData> Service => _service;
+    }
+}
