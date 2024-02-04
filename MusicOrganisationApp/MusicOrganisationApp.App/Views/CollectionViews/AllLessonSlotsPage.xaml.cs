9eb5fdf844aba145a260c0fd4370ca1c7472dd6b
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-02-04 19:37:14 +0000

    create lesson slot pages

---
+++ MusicOrganisationApp/MusicOrganisationApp.App/Views/CollectionViews/AllLessonSlotsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MusicOrganisationApp.App.Views.CollectionViews;
+
+public partial class AllLessonSlotsPage : ContentPage
+{
+	public AllLessonSlotsPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MusicOrganisationApp/MusicOrganisationApp.App/Views/EditViews/EditLessonSlotPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MusicOrganisationApp.App.Views.EditViews;
+
+public partial class EditLessonSlotPage : ContentPage
+{
+	public EditLessonSlotPage()
+	{
+		InitializeComponent();
+	}
+}
