f3a824e6ac7612e35e5cba6b279f11d22189e697
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-02-06 14:19:46 +0000

    select clashing lesson slot

--- MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/CollectionViewModels/AllLessonSlotsViewModel.cs
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/CollectionViewModels/AllLessonSlotsViewModel.cs
@@ -1,17 +1,21 @@
-﻿using MusicOrganisationApp.Lib.Services;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MusicOrganisationApp.Lib.Services;
 using MusicOrganisationApp.Lib.Tables;
 using MusicOrganisationApp.Lib.ViewModels.EditViewModels;
 using MusicOrganisationApp.Lib.ViewModels.ModelViewModels;
 
 namespace MusicOrganisationApp.Lib.ViewModels.CollectionViewModels
 {
-    public class AllLessonSlotsViewModel : CollectionViewModelBase<LessonSlotData>
+    public partial class AllLessonSlotsViewModel : CollectionViewModelBase<LessonSlotData>
     {
         public const string ROUTE = nameof(AllLessonSlotsViewModel);
 
         private static readonly List<DayOfWeek> _daysOfWeek = LessonSlotService.GetDaysOfWeek();
 
         private readonly LessonSlotService _service;
+
+        [ObservableProperty]
+        private DayOfWeek _selectedDayOfWeek = DayOfWeek.Monday;
 
         public AllLessonSlotsViewModel() : base(LessonSlotViewModel.ROUTE, EditLessonSlotViewModel.ROUTE)
         {
@@ -21,5 +25,11 @@ namespace MusicOrganisationApp.Lib.ViewModels.CollectionViewModels
         public List<DayOfWeek> DaysOfWeek => _daysOfWeek;
 
         protected override IService<LessonSlotData> Service => _service;
+
+        async partial void OnSelectedDayOfWeekChanged(DayOfWeek value)
+        {
+            _service.DayOfWeek = value;
+            await RefreshAsync();
+        }
     }
 }
