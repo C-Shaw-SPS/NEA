a1885711e00597d07a3ff553fdf580ab975260c9
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-02-06 19:02:36 +0000

    Create EditLessonViewModel.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/EditViewModels/EditLessonViewModel.cs
@@ -0,0 +1,53 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MusicOrganisationApp.Lib.Services;
+using MusicOrganisationApp.Lib.Tables;
+using System.Collections.ObjectModel;
+
+namespace MusicOrganisationApp.Lib.ViewModels.EditViewModels
+{
+    public partial class EditLessonViewModel : EditViewModelBase<LessonData>
+    {
+        private const string _EDIT_PAGE_TITLE = "Edit lesson";
+        private const string _NEW_PAGE_TITLE = "New lesson";
+
+        private readonly LessonService _service;
+
+        [ObservableProperty]
+        private DateTime _date = DateTime.Today;
+
+        [ObservableProperty]
+        private TimeSpan _startTime;
+
+        [ObservableProperty]
+        private TimeSpan _endTime;
+
+        [ObservableProperty]
+        private string _notes = string.Empty;
+
+        [ObservableProperty]
+        private ObservableCollection<LessonData> _clashingLessonSlots = [];
+
+        [ObservableProperty]
+        private LessonData? _selectedClashingLessonSlot;
+
+        public EditLessonViewModel() : base(_EDIT_PAGE_TITLE, _NEW_PAGE_TITLE)
+        {
+            _service = new(_database);
+        }
+
+        protected override IService<LessonData> Service => _service;
+
+        protected override void SetDisplayValues()
+        {
+            Date = _value.Date;
+            StartTime = _value.StartTime;
+            EndTime = _value.EndTime;
+            Notes = _value.Notes;
+        }
+
+        protected override Task<bool> TrySetValuesToSave()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
