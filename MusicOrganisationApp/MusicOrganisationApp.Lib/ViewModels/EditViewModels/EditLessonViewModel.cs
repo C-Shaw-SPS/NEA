09ab2ebd60d3fe400737c65caeeac0d44f8e74ee
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-02-07 10:20:25 +0000

    search pupils

--- MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/EditViewModels/EditLessonViewModel.cs
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/ViewModels/EditViewModels/EditLessonViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using MusicOrganisationApp.Lib.Models;
 using MusicOrganisationApp.Lib.Services;
 using MusicOrganisationApp.Lib.Tables;
@@ -14,7 +15,9 @@ namespace MusicOrganisationApp.Lib.ViewModels.EditViewModels
         private const string _EDIT_PAGE_TITLE = "Edit lesson";
         private const string _NEW_PAGE_TITLE = "New lesson";
 
-        private readonly LessonService _service;
+        private readonly LessonService _lessonService;
+        private readonly PupilService _pupilService;
+        private readonly AsyncRelayCommand _searchPupilsCommand;
 
         [ObservableProperty]
         private DateTime _date;
@@ -22,25 +25,71 @@ namespace MusicOrganisationApp.Lib.ViewModels.EditViewModels
         [ObservableProperty]
         private string _notes = string.Empty;
 
+        [ObservableProperty]
+        private string _pupilName = string.Empty;
+
+        [ObservableProperty]
+        private string _pupilSearchText = string.Empty;
+
+        [ObservableProperty]
+        private ObservableCollection<Pupil> _pupils = [];
+
+        [ObservableProperty]
+        private Pupil? _selectedPupil;
+
         public EditLessonViewModel() : base(LessonViewModel.ROUTE, _EDIT_PAGE_TITLE, _NEW_PAGE_TITLE)
         {
-            _service = new(_database);
+            _lessonService = new(_database);
+            _pupilService = new(_database);
+            _searchPupilsCommand = new(SearchPupilsAsync);
         }
 
-        protected override LessonServiceBase<Lesson, LessonData> LessonService => _service;
+        protected override LessonServiceBase<Lesson, LessonData> LessonService => _lessonService;
 
         protected override object SelectedDateObject => Date;
 
-        protected override void SetNonTimeValuesToSave()
+        public AsyncRelayCommand SearchPupilsCommand => _searchPupilsCommand;
+
+        private async Task SearchPupilsAsync()
+        {
+            IEnumerable<Pupil> pupils = await _pupilService.SearchAsync(PupilSearchText, nameof(Pupil.Name));
+            ResetCollection(Pupils, pupils);
+        }
+
+        protected override bool TrySetNonTimeValuesToSave()
         {
             _value.Date = Date;
             _value.Notes = Notes;
+            bool canSave = TrySavePupil();
+            return canSave;
+        }
+
+        private bool TrySavePupil()
+        {
+            if (SelectedPupil is not null)
+            {
+                _value.PupilId = SelectedPupil.Id;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         protected override void SetDisplayNonTimeValues()
         {
             Date = _value.Date;
             Notes = _value.Notes;
+            PupilName = _value.PupilName;
+        }
+
+        partial void OnSelectedPupilChanged(Pupil? value)
+        {
+            if (value is not null)
+            {
+                PupilName = value.Name;
+            }
         }
     }
 }
