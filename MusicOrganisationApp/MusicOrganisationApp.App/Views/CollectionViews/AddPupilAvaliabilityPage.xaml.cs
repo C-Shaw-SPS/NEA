b6629b90b101698f6460a14d727890e4eb8edbeb
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-02-08 13:23:38 +0000

    pupil availability pages

---
+++ MusicOrganisationApp/MusicOrganisationApp.App/Views/CollectionViews/AddPupilAvaliabilityPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MusicOrganisationApp.App.Views.CollectionViews;
+
+public partial class AddPupilAvaliabilityPage : ContentPage
+{
+	public AddPupilAvaliabilityPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MusicOrganisationApp/MusicOrganisationApp.App/Views/CollectionViews/PupilAvaliabilityPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MusicOrganisationApp.App.Views.CollectionViews;
+
+public partial class PupilAvaliabilityPage : ContentPage
+{
+	public PupilAvaliabilityPage()
+	{
+		InitializeComponent();
+	}
+}
