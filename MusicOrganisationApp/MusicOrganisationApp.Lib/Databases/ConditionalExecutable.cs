55c252b48542f3804d527c4f1d5a02b224c5e934
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-02-08 18:56:43 +0000

    fix comparisons

--- MusicOrganisationApp/MusicOrganisationApp.Lib/Databases/ConditionalExecutable.cs
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/Databases/ConditionalExecutable.cs
@@ -6,7 +6,8 @@
 
         public void AddWhereEqual<TTable>(string column, object? value) where TTable : ITable
         {
-            AddCondition<TTable>(ISqlExecutable.WHERE, column, value, ISqlExecutable.EQUALS);
+            string comparison = value is null ? ISqlExecutable.IS : ISqlExecutable.EQUALS;
+            AddCondition<TTable>(ISqlExecutable.WHERE, column, value, comparison);
         }
 
         public void AddWhereLike<TTable>(string column, string value) where TTable : ITable
@@ -14,16 +15,21 @@
             AddCondition<TTable>(ISqlExecutable.WHERE, column, $"%{value}%", ISqlExecutable.LIKE);
         }
 
+        public void AddWhereGreaterOrEqual<TTable>(string column, object value) where TTable : ITable
+        {
+            AddCondition<TTable>(ISqlExecutable.WHERE, column, value, ISqlExecutable.GREATER_OR_EQUAL);
+        }
+
         public void AddAndEqual<TTable>(string column, object? value) where TTable : ITable
         {
-            string condition = value is null ? ISqlExecutable.IS : ISqlExecutable.EQUALS;
-            AddCondition<TTable>(ISqlExecutable.AND, column, value, condition);
+            string comparison = value is null ? ISqlExecutable.IS : ISqlExecutable.EQUALS;
+            AddCondition<TTable>(ISqlExecutable.AND, column, value, comparison);
         }
 
         public void AddOrEqual<TTable>(string column, object? value) where TTable : ITable
         {
-            string condition = value is null ? ISqlExecutable.IS : ISqlExecutable.EQUALS;
-            AddCondition<TTable>(ISqlExecutable.OR, column, value, condition);
+            string comparison = value is null ? ISqlExecutable.IS : ISqlExecutable.EQUALS;
+            AddCondition<TTable>(ISqlExecutable.OR, column, value, comparison);
         }
 
         public void AddAndGreaterThan<TTable>(string column, object value) where TTable : ITable
@@ -48,13 +54,13 @@
 
         public void AddAndNotEqual<TTable>(string column, object? value) where TTable : ITable
         {
-            string condition = value is null ? ISqlExecutable.IS_NOT : ISqlExecutable.NOT_EQUAL;
-            AddCondition<TTable>(ISqlExecutable.AND, column, value, condition);
+            string comparison = value is null ? ISqlExecutable.IS_NOT : ISqlExecutable.NOT_EQUAL;
+            AddCondition<TTable>(ISqlExecutable.AND, column, value, comparison);
         }
 
-        private void AddCondition<TTable>(string keyword, string column, object? value, string condition) where TTable : ITable
+        private void AddCondition<TTable>(string keyword, string column, object? value, string comparison) where TTable : ITable
         {
-            _conditions.Add((keyword, TTable.TableName, column, SqlFormatting.FormatSqlValue(value), condition));
+            _conditions.Add((keyword, TTable.TableName, column, SqlFormatting.FormatSqlValue(value), comparison));
         }
 
         public abstract string GetSql();
