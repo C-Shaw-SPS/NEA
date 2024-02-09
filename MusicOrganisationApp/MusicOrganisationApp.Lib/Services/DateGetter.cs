fe4d2ee75ecdf48f08eedfc640bbe4f305ef4364
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-02-09 17:13:10 +0000

    Create DateGetter.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/Services/DateGetter.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicOrganisationApp.Lib.Services
+{
+    public static class DateGetter
+    {
+        public static DateTime FromDayOfWeek(DateTime dateInWeek, DayOfWeek dayOfWeek)
+        {
+            int dayDiff = dayOfWeek - dateInWeek.DayOfWeek;
+            DateTime dayInWeek = dateInWeek.AddDays(dayDiff);
+            return dayInWeek;
+        }
+    }
+}
