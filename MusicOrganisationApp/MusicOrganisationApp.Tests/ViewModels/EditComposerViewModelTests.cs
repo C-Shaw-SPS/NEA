c880d2c6c3e6799667489d2d96544db0a5831884
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-02-28 19:02:58 +0000

    Create EditComposerViewModelTests.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Tests/ViewModels/EditComposerViewModelTests.cs
@@ -0,0 +1,81 @@
+﻿using MusicOrganisationApp.Lib.Databases;
+using MusicOrganisationApp.Lib.Tables;
+using MusicOrganisationApp.Lib.ViewModels.EditViewModels;
+
+namespace MusicOrganisationApp.Tests.ViewModels
+{
+    public class EditComposerViewModelTests
+    {
+        [Fact]
+        public async Task TestValidSaveAsync()
+        {
+            EditComposerViewModel viewModel = new(nameof(TestValidSaveAsync), true)
+            {
+                Name = "Bach",
+                BirthYear = "1685",
+                DeathYear = "1750"
+            };
+            await viewModel.TrySaveCommand.ExecuteAsync(null);
+            Assert.False(viewModel.IsCurrentViewModel);
+            Assert.Empty(viewModel.NameError);
+            Assert.Empty(viewModel.BirthYearError);
+            Assert.Empty(viewModel.DeathYearError);
+        }
+
+        [Fact]
+        public async Task TestSaveWithInvalidNameAsync()
+        {
+            EditComposerViewModel viewModel = new(nameof(TestSaveWithInvalidNameAsync), true);
+            await viewModel.TrySaveCommand.ExecuteAsync(null);
+            Assert.True(viewModel.IsCurrentViewModel);
+            Assert.NotEmpty(viewModel.NameError);
+        }
+
+        [Fact]
+        public async Task TestSaveWithInvalidDatesAsync()
+        {
+            EditComposerViewModel viewModel = new(nameof(TestSaveWithInvalidDatesAsync), true)
+            {
+                Name = "Bach",
+                BirthYear = "1750",
+                DeathYear = "1685"
+            };
+            await viewModel.TrySaveCommand.ExecuteAsync(null);
+            Assert.True(viewModel.IsCurrentViewModel);
+            Assert.NotEmpty(viewModel.DeathYearError);
+        }
+
+        [Fact]
+        public void TestNumericDateValidation()
+        {
+            string validBirthYear = "2024";
+            string invalidBirthYear = "hello world!";
+
+            EditComposerViewModel viewModel = new(nameof(TestNumericDateValidation), true)
+            {
+                BirthYear = validBirthYear
+            };
+            Assert.Equal(validBirthYear, viewModel.BirthYear);
+            viewModel.BirthYear = invalidBirthYear;
+            Assert.Equal(validBirthYear, viewModel.BirthYear);
+        }
+
+        [Fact]
+        public async Task TestEditExistingComposerAsync()
+        {
+            DatabaseConnection database = new(nameof(TestEditExistingComposerAsync));
+            await database.ResetTableAsync(ExpectedViewModels.Composers);
+            Composer expectedComposer = ExpectedViewModels.Composers[0];
+            EditComposerViewModel viewModel = new(nameof(TestEditExistingComposerAsync), true);
+            Dictionary<string, object> query = new()
+            {
+                [EditViewModelBase.ID_PARAMETER] = expectedComposer.Id
+            };
+            await viewModel.ApplyQueryAttributesAsync(query);
+            Assert.Equal(expectedComposer.Name, viewModel.Name);
+            Assert.Equal(expectedComposer.BirthYear.ToString(), viewModel.BirthYear);
+            Assert.Equal(expectedComposer.DeathYear.ToString(), viewModel.DeathYear);
+            Assert.Equal(expectedComposer.Era, viewModel.Era);
+        }
+    }
+}
