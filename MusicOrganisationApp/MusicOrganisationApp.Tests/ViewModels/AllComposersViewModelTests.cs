0fd5694953d06ec804c420e5458d8a847bdd046f
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-02-29 11:22:19 +0000

    Create AllComposersViewModelTests.cs

---
+++ MusicOrganisationApp/MusicOrganisationApp.Tests/ViewModels/AllComposersViewModelTests.cs
@@ -0,0 +1,35 @@
+﻿using MusicOrganisationApp.Lib.Databases;
+using MusicOrganisationApp.Lib.Tables;
+using MusicOrganisationApp.Lib.ViewModels.CollectionViewModels;
+
+namespace MusicOrganisationApp.Tests.ViewModels
+{
+    public class AllComposersViewModelTests
+    {
+        [Fact]
+        public async Task TestEmptySearchAsync()
+        {
+            DatabaseConnection database = new(nameof(TestEmptySearchAsync));
+            await database.ResetTableAsync(ExpectedViewModels.Composers);
+            AllComposersViewModel viewModel = new(nameof(TestEmptySearchAsync), true)
+            {
+                SearchText = string.Empty
+            };
+            await viewModel.SearchCommand.ExecuteAsync(null);
+            CollectionAssert.EqualContents(ExpectedViewModels.Composers, viewModel.Collection);
+        }
+
+        [Fact]
+        public async Task TestSearchAsync()
+        {
+            DatabaseConnection database = new(nameof(TestSearchAsync));
+            await database.ResetTableAsync(ExpectedViewModels.Composers);
+            AllComposersViewModel viewModel = new(nameof(TestSearchAsync), true);
+            foreach (Composer expectedComposer in ExpectedViewModels.Composers)
+            {
+                await viewModel.SearchCommand.ExecuteAsync(null);
+                Assert.Contains(expectedComposer, viewModel.Collection);
+            }
+        }
+    }
+}
