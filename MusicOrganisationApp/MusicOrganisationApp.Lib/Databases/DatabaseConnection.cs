c1e1c26a70053669b51bc2cd14be2bd37c4bdf9a
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-03-06 15:50:51 +0000

    create query from string

--- MusicOrganisationApp/MusicOrganisationApp.Lib/Databases/DatabaseConnection.cs
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/Databases/DatabaseConnection.cs
@@ -28,7 +28,13 @@ namespace MusicOrganisationApp.Lib.Databases
         {
             await CreateTablesAsync(sqlQuery.Tables);
             string sql = sqlQuery.GetSql();
-            IEnumerable<T> result = await _connection.QueryAsync<T>(sql);
+            IEnumerable<T> result = await QueryAsync<T>(sql);
+            return result;
+        }
+
+        public async Task<IEnumerable<T>> QueryAsync<T>(string sqlQuery) where T : class, new()
+        {
+            IEnumerable<T> result = await _connection.QueryAsync<T>(sqlQuery);
             return result;
         }
 
