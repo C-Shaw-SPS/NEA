e22330e34c382467bb6eaee06841f89b2168fda7
C-Shaw-SPS <shawc@stpaulsschool.org.uk>
2024-03-06 12:11:11 +0000

    Update WebReader.cs

--- MusicOrganisationApp/MusicOrganisationApp.Lib/Json/WebReader.cs
+++ MusicOrganisationApp/MusicOrganisationApp.Lib/Json/WebReader.cs
@@ -2,7 +2,7 @@
 {
     internal static class WebReader
     {
-        public async static Task<(bool suceeded, string text)> DownloadText(string url)
+        public async static Task<(bool suceeded, string text)> TryDownloadTextAsync(string url)
         {
             using (HttpClient client = new())
             {
